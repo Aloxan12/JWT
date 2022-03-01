24d504a6fee7e16ba9a70f9bef95e3c74ef0e993
Aloxan12 <kvakvaaak@mail.ru>
2022-03-02 00:26:49 +0300

    sasa

--- client/src/Pages/Header/Header.tsx
+++ client/src/Pages/Header/Header.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 export const Header =()=>{
     return(
         <div className={styles.mainHeaderWrap}>
-            ujkjdf
+            <div className={styles.mainHeaderTitle}>Название сайта</div>
+            <div className={styles.mainHeaderAuth}>
+                <NavLink to={'/login'}>Войти</NavLink>
+                <NavLink to={'/registration'}>Регитсрация</NavLink>
+            </div>
         </div>
     )
 }
