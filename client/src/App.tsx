628676bab5b93282a8a83a0a608f218f003f7720
Aloxan12 <kvakvaaak@mail.ru>
2022-03-13 01:45:21 +0300

    sasa

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import {NavLink} from 'react-router-dom';
+import React, {useState} from 'react';
 import styles from './App.module.css';
+import {BurgerMenu, IItemsRoute} from "./Common/Components/BurgerMenu/BurgerMenu";
 
 
 function App() {
-
+    const [menuActive, setMenuActive] = useState(false)
+    const itemsRoute: IItemsRoute[] = [{value: 'название ссылки', href:'путь', icon:'icon'}, {value: 'название ссылки', href:'путь', icon:'icon'}]
     return (
         <div className={styles.appWrap}>
             <div>
                 <nav>
-                    <div className={styles.burgerBtn}>
+                    <div className={styles.burgerBtn} onClick={()=> setMenuActive(prevState => !prevState)}>
                         <span />
                     </div>
                 </nav>
             </div>
-            <NavLink to={"/registration"}>Регистрация</NavLink>
+            <BurgerMenu active={menuActive} setActive={setMenuActive} header="Меню" items={itemsRoute} />
         </div>
     );
 }
--- client/src/Common/Components/BurgerMenu/BurgerMenu.tsx
+++ client/src/Common/Components/BurgerMenu/BurgerMenu.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import styles from './BurgerMenu.module.css'
 
-interface IBurgerMenu{
-    headers: any
-    items: any
+export interface IItemsRoute {
+    href: string,
+    value: string,
+    icon: string
 }
 
-export const BurgerMenu = ({items, headers}:IBurgerMenu) => {
-    return (
-        <div>
+interface IBurgerMenu {
+    active: boolean
+    setActive:(value: boolean) => void
+    header: string
+    items: IItemsRoute[]
+}
 
+export const BurgerMenu = ({items, header, active, setActive }: IBurgerMenu) => {
+    return (
+        <div className={active ? `${styles.menu} ${styles.active}` : styles.menu}>
+            <div className={styles.blur}/>
+            <div className={styles.menuContent}>
+                <div className={styles.menuHeader}>{header}</div>
+                <ul>
+                    {items.map((item: any) =>
+                        <li>
+                            <a href={item.href}>{item.value}</a>
+                            <span>{item.icon}</span>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </div>
     );
 };
