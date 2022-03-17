a7de7a51b243ab2ff4f4bae4bdfb5d2e57a2a5d8
Aloxan12 <kvakvaaak@mail.ru>
2022-03-18 00:02:56 +0300

    client part

--- client/src/redux/middleware/error-middleware.ts
+++ client/src/redux/middleware/error-middleware.ts
@@ -1,12 +1,13 @@
 import {isRejectedWithValue, Middleware, MiddlewareAPI } from "@reduxjs/toolkit"
 import {ToastWrapper, ToastWrapperType} from "../../Common/Components/ToastWrapper/ToastWrapper";
+import {logout} from "../Reducers/authReducer/authReducer";
 
 export const rtkErrorMiddleware: Middleware =
     (api: MiddlewareAPI) => (next) => (action) => {
         if (isRejectedWithValue(action)) {
             console.log('action.payload', action.payload)
             if (action.payload.status === 401) {
-                // api.dispatch(deleteCredentials())
+                api.dispatch(logout())
             }
             ToastWrapper({
                 msg: JSON.stringify(
