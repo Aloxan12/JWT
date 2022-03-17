8bb53cbeef2da60be728f3c43bd351b38b2d0267
Aloxan12 <kvakvaaak@mail.ru>
2022-03-18 00:38:05 +0300

    post bek

--- server/models/post-model.js
+++ server/models/post-model.js
@@ -2,8 +2,8 @@ const {Schema, model} = require('mongoose')
 
 const PostSchema = new Schema({
     message: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User'},
-    publicDate: {type: Date, required: false},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    publicDate: {type: Date, required: true},
 }, {collection : 'posts' })
 
 module.exports = model('Post', PostSchema)
--- server/service/post-service.js
+++ server/service/post-service.js
@@ -1,4 +1,6 @@
 const PostModel = require('../models/post-model')
+const ApiError = require("../exeptions/api-error");
+const PostDto = require("../dtos/post-dto");
 require('dotenv').config()
 
 
@@ -6,6 +8,16 @@ class PostService{
     async getAllPosts(){
         const posts = await PostModel.find()
         return posts
+    }
+    async createPosts(message, author, publicDate){
+        if(message.length > 5000){
+            throw ApiError.BadRequest(`Текст не олжен привышать 5000 символов`)
+        }
+        const post = await PostModel.create({message, author, publicDate})
+        const postDto = new PostDto(post)
+        return {
+            post: postDto
+        }
     }
 }
 
