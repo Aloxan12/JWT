5aa15026e4de44967f753b324bf25afd061096f3
Aloxan12 <kvakvaaak@mail.ru>
2022-03-22 00:15:37 +0300

    client part

--- client/src/AppRoute.tsx
+++ client/src/AppRoute.tsx
@@ -1,37 +1,39 @@
 import App from "./App";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Route, Routes, useLocation, useMatch, useNavigate} from 'react-router-dom'
 import {Registration} from "./Pages/Registration/Registration";
 import {Header} from "./Pages/Header/Header";
 import {ToastContainer} from "react-toastify";
 import React from "react";
 import {Login} from "./Pages/Login/Login";
 import {ComponentsShow} from "./Pages/ComponentsShow/ComponentsShow";
+import {useSelector} from "react-redux";
+import {RootState} from "./redux/store";
 
 export const routes = [
     {
         id: 'Main',
         path: '/',
         // exact: true,
-        component: <App />,
+        component: <App/>,
         title: 'Главная'
     },
     {
         id: 'Registration',
         path: '/registration',
         // exact: true,
-        component: <Registration />
+        component: <Registration/>
     },
     {
         id: 'Login',
         path: '/login',
         // exact: true,
-        component: <Login />
+        component: <Login/>
     },
     {
         id: 'ComponentsShow',
         path: '/componentsShow',
         // exact: true,
-        component: <ComponentsShow />
+        component: <ComponentsShow/>
     },
 ];
 
@@ -62,6 +64,14 @@ export const routesForMenu = [
     }
 ];
 
+export const AppRedirect = ({path = '/'}: {
+    path?: string
+}) => {
+    const location = useLocation()
+    return <Navigate to={path} state={{from: location}} replace/>
+}
+
+
 export const getRouteConfig = (id: string) => {
     const route = routes.find(route => route.id === id);
 
@@ -72,20 +82,37 @@ export const getRouteConfig = (id: string) => {
     }
 }
 
+// const IsAuthWrap = ({children}: { children: JSX.Element }) => {
+//     const isAuth = useSelector<RootState, boolean>(
+//         (state) => state.auth.isAuth,
+//     )
+//     const navigate = useNavigate();
+//
+//     const isSameUrl = !!useMatch('registration')
+//
+//     if (!isAuth) {
+//         return isSameUrl ? <AppRedirect path={'/registration'} /> : <AppRedirect path={'/login'} />
+//     }
+//
+//     return children
+// }
+
 
 export const AppRoutes = () => {
     return (
         <BrowserRouter>
-            <Header itemsRoute={routesForMenu} />
-            <ToastContainer/>
-            <Routes>
-                {routes.map(route => {
-                    const {id, component, path} = route;
-                    return (
-                        <Route key={id} path={path} element={component} />
-                    )
-                })}
-            </Routes>
+            <React.Fragment>
+                <Header itemsRoute={routesForMenu}/>
+                <ToastContainer/>
+                <Routes>
+                    {routes.map(route => {
+                        const {id, component, path} = route;
+                        return (
+                            <Route key={id} path={path} element={component}/>
+                        )
+                    })}
+                </Routes>
+            </React.Fragment>
         </BrowserRouter>
     )
 }
