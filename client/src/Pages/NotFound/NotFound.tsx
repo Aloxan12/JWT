6dd92827f057e84d91a7099e6bc71d73fd39e93a
Aloxan12 <kvakvaaak@mail.ru>
2022-03-27 02:24:35 +0300

    client part

--- client/src/Pages/NotFound/NotFound.tsx
+++ client/src/Pages/NotFound/NotFound.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import styles from './NotFound.module.css'
 import paper from '../../utils/images/paper.png'
 import {AppButton} from "../../Common/Components/AppButton/AppButton";
+import {MatrixRain} from "./MatrixRain";
 
 export const NotFound = () => {
     return (
         <div className={styles.NotFoundWrap}>
             <img src={paper} alt={'not found'}/>
-            <div>404</div>
+            <div className={styles.NotFoundText}>404</div>
+            <MatrixRain />
             <div className={styles.onGoMainBtn}>
-                <AppButton onClick={()=>{}} text='На главную'/>
+                <div className={styles.redTablet}>
+                </div>
             </div>
             <div className={styles.onClosesBtn}>
-                <AppButton onClick={()=>{}} text='Закрыть вкладку'/>
+                <div className={styles.blueTablet}>
+                </div>
             </div>
         </div>
     );
