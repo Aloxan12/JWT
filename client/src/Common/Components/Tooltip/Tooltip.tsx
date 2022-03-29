ca195df4a8c621599f3fc6726a98dc45b4cecd9f
Aloxan12 <kvakvaaak@mail.ru>
2022-03-30 00:11:25 +0300

    client part

---
+++ client/src/Common/Components/Tooltip/Tooltip.tsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import styles from './Tooltip.module.css'
+
+type TooltipType = {
+    delay?: number
+    direction?: string
+    content?: string
+    children?: React.ReactNode
+    textColor?: 'green'
+}
+
+export const Tooltip = (props: TooltipType)=>{
+    let timeout: ReturnType<typeof setTimeout>;
+    const [active, setActive] = useState(false);
+
+    const showTip = () => {
+        timeout = setTimeout(() => {
+            setActive(true);
+        }, props.delay || 400);
+    };
+
+    const hideTip = () => {
+        clearInterval(timeout);
+        setActive(false);
+    };
+
+
+    return (
+        <div
+            className={styles.TooltipWrapper}
+            // When to show the tooltip
+            onMouseEnter={showTip}
+            onMouseLeave={hideTip}
+        >
+            {/* Wrapping */}
+            {props.children}
+            {active && (
+                <div className={`${styles.TooltipTip} ` + `${props.direction ? styles[props.direction] : styles["top"]} ` + `${props.textColor}`}>
+                    {/* Content */}
+                    {props.content}
+                </div>
+            )}
+        </div>
+    )
+}
--- client/src/Pages/NotFound/NotFound.tsx
+++ client/src/Pages/NotFound/NotFound.tsx
@@ -1,46 +1,58 @@
 import React from 'react';
 import styles from './NotFound.module.css'
 import paper from '../../utils/images/paper.png'
+import {Tooltip} from "../../Common/Components/Tooltip/Tooltip";
+import {useNavigate} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
 
 const arrNotFoundText = [
     'page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404',
-    'page not found 404 page not found 404 page not error error 404 404 404 404 404 404 error error found 404 page not found 404 page not found 404 page not found 404',
-    'error page not found error page not found error page not found e404 404 404 404 404 404 404 not found error page not found',
-    '404 404 404 404 404 404 404 page not found 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 error error error error 404 404 404 404 error error error error 404 404 404 404 404 404 404 404 ',
+    'page not found 404 page not found 404 page not error error 404 404 404 404 404 404 error error found 404 page not found 404 page not found 404 page not found 404 error 404 error page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page 404 404 404 404 404 404 404 not found 404',
+    'error page not found error page not found error page not found  e404 404 404 404 404 404 404 not found error page not found e404 404 404 404 404 404 404 not found error page not found e404 404 404 404 404 404 404 not found error page not found e404 404 404 404 404 404 404 not found error page not found',
+    '404 404 404 404 404 404 404 page not found 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 404 error error error error 404 404 404 404 error error error error 404 404 404 404 404 404 404 404  404 page not found 404 page not found 404  404 page not found 404 page not found 404',
     'page not found 404 page not found 404 page not found 404  404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404',
     'error error error error error error page not found 404 page not found error error error 404 page not found 404 page not found 404 page not found 404 page not found 404',
-    '404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404',
+    '404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 404 page not found 404 page not found 404 page not found 404 page 404 404 404 404 404 404 404 not found 404 404 page not found 404 page not found 404 page not found 404 page 404 404 404 404 404 404 404 not found 404 ',
     'found error page not found error 404 404 404 404 404 404 404 page not found error 404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404',
     'page not found 404 page not found 404 page not found 404  404 404 404 404 404 404 404 404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 404 404 404 404 404 404 404',
     '  404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404',
-    '404 page not found 404 page not found 404 page not found 404 page not 404 page not found 404 found 404 page not found 404 page not found 404',
+    '404 page not found 404 page not found 404 page not found 404 page not 404 page not found 404 found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404',
     'error error page not found 404 page not found 404 page not found 404 page not found 404 404 404 404 404 404 404 404 page not found 404 page not found 404',
-    '404 page not found 404 404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404',
-    'page not found 404 page not found 404 page not found 404 page not found 404 404 page not found 404 page not found 404 page not found 404',
+    '404 page not found 404 404 404 404 404 404 404 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404',
+    'page not found 404 page not found 404 page not found 404 page not found 404 404 page not found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404  404 found 404 page not found 404 page not found 404',
     'error 404 error page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page 404 404 404 404 404 404 404 not found 404',
-    ' 404 page not found 404 page not found 404 page not found 404 page not found 404 404 page 404 page not found 404 404 404 404 404 404 404 404 not found 404 page not found 404 page not found 404 page not found 404',
+    ' 404 page not found 404 page not found 404 page not found 404 page not found 404 404 page 404 page not found 404 404 404 404 404 404 404 404 not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found 404 page not found',
 ]
 
 export const NotFound = () => {
+    const navigate = useNavigate()
+    const isAuth = useSelector<RootState, boolean>(state => state.auth.isAuth)
     return (
         <div className={styles.NotFoundWrap}>
             <img src={paper} alt={'not found'}/>
-            {/*<div className={styles.NotFoundText}>404</div>*/}
+            <div className={styles.NotFoundText}>404</div>
             <div className={styles.runningStringBlock}>
                 {arrNotFoundText.map((item, i) => <div>
-                    <span key={`not found key ${i}`}>{item}</span>
+                    <span className={styles.runningStringText} key={`not found key ${i}`}>{item}</span>
                 </div>)}
                 <div>
                     <span>not found 404</span>
                 </div>
             </div>
             <div className={styles.onGoMainBtn}>
-                <div className={styles.redTablet}>
-                </div>
+                <Tooltip content="Вернуться на главную">
+                    <div className={styles.redTablet} onClick={()=>navigate(isAuth ? '/' : '/login')}>
+                    </div>
+                </Tooltip>
             </div>
             <div className={styles.onClosesBtn}>
-                <div className={styles.blueTablet}>
-                </div>
+                <Tooltip content="Покинуть сайт">
+                    <a href="https://www.google.com/">
+                        <div className={styles.blueTablet}>
+                        </div>
+                    </a>
+                </Tooltip>
             </div>
         </div>
     );
