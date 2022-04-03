b55d188e50ed81011dcc33ceff4d904100c1421d
Aloxan12 <kvakvaaak@mail.ru>
2022-04-04 00:09:05 +0300

    client part

--- client/src/Common/Components/Modal/Modal.tsx
+++ client/src/Common/Components/Modal/Modal.tsx
@@ -4,13 +4,21 @@ import style from "./Modal.module.css";
 type ModalType = {
     active: boolean
     setActive: (value: boolean) => void
+    title?: string
 }
 
-export const Modal: React.FC<ModalType> = ({active, setActive, children}) => {
+export const Modal: React.FC<ModalType> = ({active, setActive, children, title}) => {
     return (
-        <div className={active ? `${style.modal} + '' + ${style.active}` : style.modal} onClick={() => setActive(false)}>
+        <div className={active ? `${style.modal} + '' + ${style.active}` : style.modal}
+             onClick={() => setActive(false)}>
             <div className={active ? `${style.modalContent} + '' + ${style.active}` : style.modalContent}>
-                {children}
+                {title ?
+                    <React.Fragment>
+                        <div className={style.headerBlock}>{title}</div>
+                        <div className={style.contentBlock}>{children}</div>
+                    </React.Fragment>
+                    :
+                    <div>{children}</div>}
             </div>
         </div>
     )
@@ -19,7 +27,7 @@ export const ModalView: React.FC = () => {
     const [active, setActive] = useState<boolean>(false)
     return (
         <div>
-            <button onClick={()=> setActive(true)}>+</button>
+            <button onClick={() => setActive(true)}>+</button>
             <Modal active={active} setActive={setActive}>Модалка</Modal>
         </div>
     )
