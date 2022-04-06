7154b1454c6835d4a66f63d9fbe4e8b4b42d2dff
Aloxan12 <kvakvaaak@mail.ru>
2022-04-07 00:23:28 +0300

    client part

--- client/src/Common/Components/AppInput/AppInput.tsx
+++ client/src/Common/Components/AppInput/AppInput.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 
 interface IAppInput{
     label: string
     type?: string
+    value?: string
+    onChange: (value: ChangeEvent<HTMLInputElement>) => void
 }
 
-export const AppInput = ({label, type = 'text'}: IAppInput) => {
+export const AppInput = ({label, type = 'text', value, onChange}: IAppInput) => {
     return (
         <div>
             {label && <label>{label}</label>}
             <div>
-                <input type={type}/>
+                <input value={value} type={type} onChange={onChange}/>
             </div>
         </div>
     );
