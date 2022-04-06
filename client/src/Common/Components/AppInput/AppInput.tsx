9f6d4458873e7ccdbd62c27be0c9a8d894b713f1
Aloxan12 <kvakvaaak@mail.ru>
2022-04-07 00:29:49 +0300

    client part

--- client/src/Common/Components/AppInput/AppInput.tsx
+++ client/src/Common/Components/AppInput/AppInput.tsx
@@ -1,19 +1,22 @@
 import React, {ChangeEvent} from 'react';
+import styles from './AppInput.module.css'
 
 interface IAppInput{
-    label: string
+    label?: string
     type?: string
-    value?: string
+    value: string
     onChange: (value: ChangeEvent<HTMLInputElement>) => void
+    error?: string | null
 }
 
-export const AppInput = ({label, type = 'text', value, onChange}: IAppInput) => {
+export const AppInput = ({label, type = 'text', value, onChange, error}: IAppInput) => {
     return (
-        <div>
+        <div className={styles.appInputWrap}>
             {label && <label>{label}</label>}
             <div>
                 <input value={value} type={type} onChange={onChange}/>
             </div>
+            {error && <div>{error}</div>}
         </div>
     );
 }
