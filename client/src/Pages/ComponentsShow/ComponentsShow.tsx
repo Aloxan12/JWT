d7e64f0772827d8a559c45db8e1dd5656b8cd254
Aloxan12 <kvakvaaak@mail.ru>
2022-04-09 01:18:56 +0300

    client part

--- client/src/Pages/ComponentsShow/ComponentsShow.tsx
+++ client/src/Pages/ComponentsShow/ComponentsShow.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {AppButton} from "../../Common/Components/AppButton/AppButton";
 import {AppTrash} from "../../Common/Components/AppTrash/AppTrash";
 import styles from './ComponentsShow.module.css'
+import {AppInput} from "../../Common/Components/AppInput/AppInput";
 
 export const ComponentsShow = () => {
     return (
         <div className={styles.ComponentsShowWrapper}>
             <AppButton onClick={(e)=> console.log(e.currentTarget)} />
             <AppTrash deleteHandler={()=>console.log('что-то удалил')} />
+            <AppInput
+                value={''}
+                onChange={(e)=>console.log(e)}
+                label={'label'}
+                error={'error'} />
         </div>
     );
 };
