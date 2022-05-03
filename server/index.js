30e9fc7f21d73d0608b1156bd02484ee4c9e122f
Aloxan12 <kvakvaaak@mail.ru>
2022-05-03 17:26:17 +0300

    image bek

--- server/index.js
+++ server/index.js
@@ -3,16 +3,19 @@ const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
-// const router = require('router/index.js')
+
 const router = require('./router/index')
 const errorMiddleware = require('./middlewares/error-middleware')
 const multer = require('multer')
 
+path = require('path')
+
 const ImageModel = require('./models/image-model')
+const UserModel = require("./models/user-model");
 
 const Storage = multer.diskStorage({
     destination: 'uploads',
-    filename: (req, file, cb)=>{
+    filename: (req, file, cb) => {
         cb(null, file.originalname)
     }
 })
@@ -21,6 +24,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'uploads')))
 app.use(cookieParser())
 app.use(cors({credential: true, origin: process.env.CLIENT_URL}));
 app.use('/api', router)
@@ -30,29 +34,36 @@ const upload = multer({
     storage: Storage
 }).single('testImage')
 
-app.post('/upload',  (req, res)=>{
-    upload(req, res, async (err)=>{
-        if(err){
+app.post('/upload', (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
             console.log(err)
-        }else{
+        } else {
             console.log('loooog', req.file)
-            const newImage = new  ImageModel({
+            const newImage = new ImageModel({
                 name: req.body.name,
-                image:{
+                image: {
                     data: req.file.filename,
                     contentType: req.file.mimetype
                 }
             })
-            await newImage.save().then(()=> res.send('файл успешно загружен')).catch(err => console.log(err))
+            await newImage.save().then(() => res.send('файл успешно загружен')).catch(err => console.log(err))
         }
     })
 })
 
-const startApp = async ()=>{
-    try{
+app.get('/upload/:name', async (req, res)=>{
+    const name = req.params.name
+    const photo = await ImageModel.findOne({name})
+
+    return res.json(path.resolve(__dirname, name))
+})
+
+const startApp = async () => {
+    try {
         await mongoose.connect(process.env.DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
-        app.listen(PORT, ()=>console.log(`connected port - ${PORT}`))
-    }catch (e) {
+        app.listen(PORT, () => console.log(`connected port - ${PORT}`))
+    } catch (e) {
         console.log(e)
     }
 }
