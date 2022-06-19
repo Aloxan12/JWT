0ece7ba62fae3f42f68365b99cedc3f807c22794
Aloxan12 <kvakvaaak@mail.ru>
2022-06-19 12:34:51 +0300

    AppInputFilter

--- client/src/Pages/Users/Users.tsx
+++ client/src/Pages/Users/Users.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import {useGetAllUsersQuery} from "../../redux/usersApi";
 import {User} from "./User";
 import styles from './Users.module.css'
+import {AppInputFilter} from "../../Common/Components/AppInputFilter";
 
 export const Users = () => {
     const {data: users} = useGetAllUsersQuery()
     return (
         <div>
+            <div>
+                <AppInputFilter searchParam='search' />
+            </div>
             <div className={styles.UsersBlock}>
                 {users && users.map(user =>{
                     return <User user={user} />
