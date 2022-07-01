b878b30d0090bbebeb5cd1b63b956d3240624adc
Aloxan12 <kvakvaaak@mail.ru>
2022-07-02 00:11:45 +0300

    AppInputFilter

--- client/src/Hooks/useParamsControl.ts
+++ client/src/Hooks/useParamsControl.ts
@@ -1,8 +1,19 @@
 
-type UseParamsControlType<T, TKey extends keyof T> = {
+interface UseParamsControlTypeBase<T, TKey extends keyof T>  {
     paramsList:TKey[]
-    withPagination: boolean
 }
+
+interface UseParamsControlTypeWithoutPagination<T, TKey extends keyof T> extends UseParamsControlTypeBase<T, TKey>{
+    withPagination: false
+    resetPagination?: never
+}
+interface UseParamsControlTypeWithPagination<T, TKey extends keyof T> extends UseParamsControlTypeBase<T, TKey>{
+    withPagination: true
+    resetPagination?: boolean
+}
+type UseParamsControlType<T, TKey extends keyof T> =
+    UseParamsControlTypeWithoutPagination<T, TKey>
+    | UseParamsControlTypeWithPagination<T, TKey>
 
 export const useParamsControl = <T, TKey extends keyof T>({paramsList, withPagination}: UseParamsControlType<T, TKey>) => {
     return {} as T
