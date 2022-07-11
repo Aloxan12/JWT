8de60ef650267a622dd3fdff7b643863ea3dfd7b
Aloxan12 <kvakvaaak@mail.ru>
2022-07-11 15:16:08 +0300

    update user

--- client/src/Pages/PersonalAccount/components/UserStatusContainer.tsx
+++ client/src/Pages/PersonalAccount/components/UserStatusContainer.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IUser} from "../../../redux/api/dto/UserDto";
 import {useUpdateUserDetailMutation} from "../../../redux/api/usersApi";
+import styles from "../PersonalAccount.module.css";
+import {AppInput} from "../../../Common/Components/AppInput/AppInput";
 
 interface IUserStatusContainer{
     user: IUser
@@ -8,9 +10,14 @@ interface IUserStatusContainer{
 
 export const UserStatusContainer = ({user}: IUserStatusContainer) => {
     const [updateUserStatus] = useUpdateUserDetailMutation()
-    return (
-        <div>
 
+    const [editStatus, setEditStatus] = useState(false)
+    const [status, setStatus] = useState('')
+    return (
+        <div className={styles.PersonalAccountInfoItem}>
+            <span>Статус: {editStatus
+                ? <AppInput onChange={(value)=> setStatus(value)} />
+                : <b>{!!user.status ? user.status : 'Статуса нет'}</b>}</span>
         </div>
     );
 };
