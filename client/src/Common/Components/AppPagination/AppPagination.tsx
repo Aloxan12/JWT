4a3a941d89d744628b332a12ffe1050d375cbf67
Aloxan12 <kvakvaaak@mail.ru>
2022-07-17 00:58:58 +0300

    pagination

--- client/src/Common/Components/AppPagination/AppPagination.tsx
+++ client/src/Common/Components/AppPagination/AppPagination.tsx
@@ -1,11 +1,65 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useSearchParams} from "react-router-dom";
 
-const AppPagination = () => {
+function createPages(
+    pages: (number | string)[],
+    pagesCount: number,
+    currentPage: number,
+) {
+    if (pagesCount > 4) {
+        if (currentPage > 3) {
+            pages.push(1, '...')
+            for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+                pages.push(i)
+                if (i === pagesCount) break
+            }
+            if (currentPage + 2 === pagesCount) {
+                pages.push(pagesCount)
+            }
+            if (currentPage + 2 < pagesCount) {
+                pages.push('...', pagesCount)
+            }
+        } else {
+            for (let i = 1; i <= 4; i++) {
+                pages.push(i)
+                if (i === pagesCount) break
+            }
+            pages.push('...', pagesCount)
+        }
+    } else {
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+    }
+}
+
+const isPaginationResetReg = /(limit=\d+&offset=\d+)/g
+
+interface IAppPagination {
+    totalCount: number
+    limit: number
+    setLimit?: (limit: number) => void
+}
+
+const AppPagination = ({totalCount, setLimit, limit}:IAppPagination) => {
     const [searchParams, setSearchParams] = useSearchParams()
     const page = searchParams.get('page')
     const [currentPage, setCurrentPage] = useState<number>(!!page ? Number(page) : 1)
-    const pages:number[] = []
+
+    const searchParamsWithoutPagination = searchParams
+        .toString()
+        .replace(isPaginationResetReg, '')
+
+    useEffect(() => {
+        setCurrentPage(1)
+    }, [limit, searchParamsWithoutPagination])
+
+    const pageCount = Math.ceil(totalCount / limit)
+    const pages: (number | string)[] = []
+
+    createPages(pages, pageCount, currentPage)
+
+
     return (
         <div>
             {pages.map((page, index) =>{
