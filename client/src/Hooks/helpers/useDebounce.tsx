d6fefff91ce187e84ddc737bbd4ce38fc040e137
Aloxan12 <kvakvaaak@mail.ru>
2022-07-23 01:45:17 +0300

    post pagination

--- client/src/Hooks/helpers/useDebounce.tsx
+++ client/src/Hooks/helpers/useDebounce.tsx
@@ -14,3 +14,27 @@ export function useDebounce<T>(value: T, delay: number) {
 
     return debouncedValue
 }
+
+export function throttle<T>(callback: T, delay: number) {
+    let isWaiting = false
+    let savedArgs: any[] | null = null
+    let savedThis: null | any = null
+    return function wrapper(this: any, ...args: any) {
+        if (isWaiting) {
+            savedArgs = args
+            savedThis = this
+            return
+        }
+        // @ts-ignorer
+        callback.apply(this, args)
+        isWaiting = true
+        setTimeout(() => {
+            isWaiting = false
+            if (savedThis) {
+                wrapper.apply(savedThis, savedArgs!)
+                savedThis = null
+                savedArgs = args
+            }
+        }, delay)
+    }
+}
