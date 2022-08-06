e5a0108b619956cc2f4d722359dd3518740dd42f
Aloxan12 <kvakvaaak@mail.ru>
2022-08-06 11:22:24 +0300

    app table

--- client/src/Common/Components/AppTable/AppTable.tsx
+++ client/src/Common/Components/AppTable/AppTable.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 interface IHeaderData {
     title: string
@@ -7,30 +7,35 @@ interface IHeaderData {
 }
 
 type IAppTable<T> = {
-    data: T
+    data: T[]
     headerData: IHeaderData[]
 }
 
-export const AppTable = <T, TKey>({headerData}:IAppTable<T>) => {
-    const [tableData, setTableData] = useState([])
+export const AppTable = <T, TKey>({headerData, data}: IAppTable<T>) => {
+    const [tableData, setTableData] = useState<T[]>([])
+
+    useEffect(() => {
+        setTableData(data)
+    }, [data])
+
     return (
         <div>
             <table>
                 <colgroup>
-                    {headerData.map((th, index)=>{
-                        return <col width={th.colWidth} key={`table-colgroup-${index}`} />
+                    {headerData.map((th, index) => {
+                        return <col width={th.colWidth} key={`table-colgroup-${index}`}/>
                     })}
                 </colgroup>
                 <thead>
-                    {headerData.map((th, index)=>{
-                        return <div key={`th-table-${index}`}>
-                            {th.title}
-                        </div>
-                    })}
+                {headerData.map((th, index) => {
+                    return <div key={`th-table-${index}`}>
+                        {th.title}
+                    </div>
+                })}
                 </thead>
                 <tbody>
-                {tableData.map((item, index)=>{
-                    return(
+                {tableData.map((item, index) => {
+                    return (
                         <tr key={`table-row-${index}`}>
                             {}
                         </tr>
