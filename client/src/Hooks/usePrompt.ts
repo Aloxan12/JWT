0093a63974f035b2de75b235fc845f32f7ee3346
Aloxan12 <kvakvaaak@mail.ru>
2022-08-11 11:17:51 +0300

    usePrompt

---
+++ client/src/Hooks/usePrompt.ts
@@ -0,0 +1,70 @@
+import * as React from 'react'
+import { UNSAFE_NavigationContext } from 'react-router-dom'
+import type { History, Blocker, Transition } from 'history'
+import { useCallback, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
+
+export function useBlocker(blocker: Blocker, when = true): void {
+  const navigator = React.useContext(UNSAFE_NavigationContext)
+    .navigator as History
+
+  React.useEffect(() => {
+    if (!when) return
+
+    const unblock = navigator.block((tx: Transition) => {
+      const autoUnblockingTx = {
+        ...tx,
+        retry() {
+          unblock()
+          tx.retry()
+        },
+      }
+
+      blocker(autoUnblockingTx)
+    })
+
+    return unblock
+  }, [navigator, blocker, when])
+}
+
+export function useCallbackPrompt(when: boolean): (boolean | (() => void))[] {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [showPrompt, setShowPrompt] = useState(false)
+  const [lastLocation, setLastLocation] = useState<any>(null)
+  const [confirmedNavigation, setConfirmedNavigation] = useState(false)
+
+  const cancelNavigation = useCallback(() => {
+    setShowPrompt(false)
+  }, [])
+
+  const handleBlockedNavigation = useCallback(
+    (nextLocation) => {
+      if (
+        !confirmedNavigation &&
+        nextLocation.location.pathname !== location.pathname
+      ) {
+        setShowPrompt(true)
+        setLastLocation(nextLocation)
+        return false
+      }
+      return true
+    },
+    [confirmedNavigation],
+  )
+
+  const confirmNavigation = useCallback(() => {
+    setShowPrompt(false)
+    setConfirmedNavigation(true)
+  }, [])
+
+  useEffect(() => {
+    if (confirmedNavigation && lastLocation) {
+      navigate(lastLocation.location.pathname)
+    }
+  }, [confirmedNavigation, lastLocation])
+
+  useBlocker(handleBlockedNavigation, when)
+
+  return [showPrompt, confirmNavigation, cancelNavigation]
+}
