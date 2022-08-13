a60a01a2ccbc7be656a1a7fc1cd54279c1128db4
Aloxan12 <kvakvaaak@mail.ru>
2022-08-13 12:46:47 +0300

    eslint

--- client/src/Common/Components/AppTable/AppTable.tsx
+++ client/src/Common/Components/AppTable/AppTable.tsx
@@ -1,97 +1,99 @@
-import React, {useEffect, useState} from 'react';
-import {AppPagination} from "../AppPagination/AppPagination";
-import {BaseQueryDto} from "../../../redux/api/dto/BaseDto";
-import styles from './AppTable.module.css'
+import React, { useEffect, useState } from 'react';
+import { AppPagination } from '../AppPagination/AppPagination';
+import { BaseQueryDto } from '../../../redux/api/dto/BaseDto';
+import styles from './AppTable.module.css';
 
 interface IHeaderData {
-    title: string
-    sort?: string
-    colWidth?: string // px or %
+  title: string;
+  sort?: string;
+  colWidth?: string; // px or %
 }
 
 interface ITableData<T> extends BaseQueryDto {
-    results: T[] | []
+  results: T[] | [];
 }
 
 interface ISelector<TKey> {
-    propsNameToDisable?: TKey
+  propsNameToDisable?: TKey;
 }
 
 interface ISelectorName<TKey> extends ISelector<TKey> {
-    name: TKey | null
-    renderItem?: never
+  name: TKey | null;
+  renderItem?: never;
 }
 
 interface ISelectorVoid<T> extends ISelector<keyof T> {
-    name?: never
-    renderItem: (item: T) => JSX.Element | string
+  name?: never;
+  renderItem: (item: T) => JSX.Element | string;
 }
 
-type TSelector<T, TKey> = ISelectorName<TKey> | ISelectorVoid<T>
+type TSelector<T, TKey> = ISelectorName<TKey> | ISelectorVoid<T>;
 
 type IAppTable<T, TKey> = {
-    data: ITableData<T>
-    headerData: IHeaderData[]
-    tableDataSelectors: TSelector<T, TKey>[]
-}
+  data: ITableData<T>;
+  headerData: IHeaderData[];
+  tableDataSelectors: TSelector<T, TKey>[];
+};
 
-export const AppTable = <T, TKey extends keyof T>({headerData, data, tableDataSelectors}: IAppTable<T, TKey>) => {
-    const [tableData, setTableData] = useState<T[]>([])
+export const AppTable = <T, TKey extends keyof T>({
+  headerData,
+  data,
+  tableDataSelectors,
+}: IAppTable<T, TKey>) => {
+  const [tableData, setTableData] = useState<T[]>([]);
 
-    const defaultRenderItem = (item: T, name: TKey) => {
-        return <div>{item[name] ? `${item[name]}` : null}</div>
-    }
+  const defaultRenderItem = (item: T, name: TKey) => {
+    return <div>{item[name] ? `${item[name]}` : null}</div>;
+  };
 
-    useEffect(() => {
-        setTableData(data.results)
-    }, [data])
+  useEffect(() => {
+    setTableData(data.results);
+  }, [data]);
 
-    return (
-        <div className={styles.AppTableWrap}>
-            <table>
-                <colgroup>
-                    {headerData.map((th, index) => {
-                        return <col width={th.colWidth} key={`table-colgroup-${index}`}/>
-                    })}
-                </colgroup>
-                <thead>
-                {headerData.map((th, index) => {
-                    return <div key={`th-table-${index}`}>
-                        {th.title}
-                    </div>
-                })}
-                </thead>
-                <tbody>
-                {tableData.map((item, itemIndex) => {
+  return (
+    <div className={styles.AppTableWrap}>
+      <table>
+        <colgroup>
+          {headerData.map((th, index) => {
+            return <col width={th.colWidth} key={`table-colgroup-${index}`} />;
+          })}
+        </colgroup>
+        <thead>
+          {headerData.map((th, index) => {
+            return <div key={`th-table-${index}`}>{th.title}</div>;
+          })}
+        </thead>
+        <tbody>
+          {tableData.map((item, itemIndex) => {
+            return (
+              <tr key={`table-row-${itemIndex}`}>
+                {tableDataSelectors.map(
+                  (
+                    {
+                      propsNameToDisable,
+                      name,
+                      renderItem = (item) => (name ? defaultRenderItem(item, name!) : '?'),
+                    },
+                    tdIndex
+                  ) => {
                     return (
-                        <tr key={`table-row-${itemIndex}`}>
-                            {tableDataSelectors.map((
-                                {
-                                    propsNameToDisable,
-                                    name,
-                                    renderItem = (item) =>
-                                        name ? defaultRenderItem(item, name!) : '?',
-                                },
-                                tdIndex,
-                            ) => {
-                                return <td
-                                    key={`tr-${itemIndex}-td-${tdIndex}`}
-                                    className={
-                                        (item[propsNameToDisable!] as unknown) === true
-                                            ? 'set-disabled'
-                                            : ''
-                                    }
-                                >
-                                    {renderItem(item)}
-                                </td>
-                            })}
-                        </tr>
-                    )
-                })
-                }
-                </tbody>
-            </table>
-            <AppPagination totalCount={1} limit={5} />
-        </div>
-    );
+                      <td
+                        key={`tr-${itemIndex}-td-${tdIndex}`}
+                        className={
+                          (item[propsNameToDisable!] as unknown) === true ? 'set-disabled' : ''
+                        }
+                      >
+                        {renderItem(item)}
+                      </td>
+                    );
+                  }
+                )}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <AppPagination totalCount={1} limit={5} />
+    </div>
+  );
 };
