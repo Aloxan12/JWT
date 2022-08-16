bd9afd190bb72d24dc2b63aa61c31937f1812630
Aloxan12 <kvakvaaak@mail.ru>
2022-08-16 10:58:49 +0300

    eslint

--- client/src/Hooks/helpers/useDebounce.tsx
+++ client/src/Hooks/helpers/useDebounce.tsx
@@ -1,40 +1,40 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
 
 export function useDebounce<T>(value: T, delay: number) {
-    const [debouncedValue, setDebouncedValue] = useState(value)
+  const [debouncedValue, setDebouncedValue] = useState(value);
 
-    useEffect(() => {
-        const handler = setTimeout(() => {
-            setDebouncedValue(value)
-        }, delay)
-        return () => {
-            clearTimeout(handler)
-        }
-    }, [value])
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
 
-    return debouncedValue
+  return debouncedValue;
 }
 
 export function throttle<T>(callback: T, delay: number) {
-    let isWaiting = false
-    let savedArgs: any[] | null = null
-    let savedThis: null | any = null
-    return function wrapper(this: any, ...args: any) {
-        if (isWaiting) {
-            savedArgs = args
-            savedThis = this
-            return
-        }
-        // @ts-ignorer
-        callback.apply(this, args)
-        isWaiting = true
-        setTimeout(() => {
-            isWaiting = false
-            if (savedThis) {
-                wrapper.apply(savedThis, savedArgs!)
-                savedThis = null
-                savedArgs = args
-            }
-        }, delay)
+  let isWaiting = false;
+  let savedArgs: any[] | null = null;
+  let savedThis: null | any = null;
+  return function wrapper(this: any, ...args: any) {
+    if (isWaiting) {
+      savedArgs = args;
+      savedThis = this;
+      return;
     }
+    // @ts-ignorer
+    callback.apply(this, args);
+    isWaiting = true;
+    setTimeout(() => {
+      isWaiting = false;
+      if (savedThis) {
+        wrapper.apply(savedThis, savedArgs!);
+        savedThis = null;
+        savedArgs = args;
+      }
+    }, delay);
+  };
 }
