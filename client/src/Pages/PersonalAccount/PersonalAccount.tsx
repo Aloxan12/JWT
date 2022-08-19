767c4a27a56d06ee7b8cdb94097a10ab948308bd
Aloxan12 <kvakvaaak@mail.ru>
2022-08-19 13:37:35 +0300

    eslint

--- client/src/Pages/PersonalAccount/PersonalAccount.tsx
+++ client/src/Pages/PersonalAccount/PersonalAccount.tsx
@@ -1,81 +1,83 @@
-import React, {ChangeEvent, useEffect, useState} from 'react';
-import styles from './PersonalAccount.module.css'
-import {useParams} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {AppButton} from "../../Common/Components/AppButton/AppButton";
-import {
-    useGetUserDetailQuery,
-    useUploadUserAvatarMutation
-} from "../../redux/api/usersApi";
-import {checkAuthApi} from "../../redux/api/checkAuthApi";
-import {setUser} from "../../redux/Reducers/authReducer/authReducer";
-import {ChangeAvatarContainer} from "./components/ChangeAvatarContainer";
-import {ProfileInfoContainer} from "./components/ProfileInfoContainer";
-import fakeAvatar from '../../utils/images/fake_avatar.png'
-import {IUser} from '../../redux/api/dto/UserDto'
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import styles from './PersonalAccount.module.css';
+import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AppButton } from '../../Common/Components/AppButton/AppButton';
+import { useGetUserDetailQuery, useUploadUserAvatarMutation } from '../../redux/api/usersApi';
+import { checkAuthApi } from '../../redux/api/checkAuthApi';
+import { setUser } from '../../redux/Reducers/authReducer/authReducer';
+import { ChangeAvatarContainer } from './components/ChangeAvatarContainer';
+import { ProfileInfoContainer } from './components/ProfileInfoContainer';
+import fakeAvatar from '../../utils/images/fake_avatar.png';
+import { IUser } from '../../redux/api/dto/UserDto';
 
 export const PersonalAccount = () => {
-    const {id} = useParams()
-    const {data: user} = useGetUserDetailQuery({id: id!}, {skip: !id})
+  const { id } = useParams();
+  const { data: user } = useGetUserDetailQuery({ id: id! }, { skip: !id });
 
-    const [uploadUserAvatar] = useUploadUserAvatarMutation()
-    const dispatch = useDispatch()
+  const [uploadUserAvatar] = useUploadUserAvatarMutation();
+  const dispatch = useDispatch();
 
-    const [changePhoto, setChangePhoto] = useState(false)
-    const [file, setFile] = useState<null | File>(null)
-    const [inputFile, setInputFile] = useState<string>('')
+  const [changePhoto, setChangePhoto] = useState(false);
+  const [file, setFile] = useState<null | File>(null);
+  const [inputFile, setInputFile] = useState<string>('');
 
-    const handleChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files && event.target.files[0]
-        setFile(file)
+  const handleChangeFile = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files && event.target.files[0];
+    setFile(file);
+  };
+
+  useEffect(() => {
+    if (!changePhoto) {
+      setInputFile('');
+      setFile(null);
     }
+  }, [changePhoto]);
 
-    useEffect(() => {
-        if (!changePhoto) {
-            setInputFile('')
-            setFile(null)
-        }
-    }, [changePhoto])
-
-    const uploadUserAvatarHandler = async () => {
-        if (!!file && !!id) {
-            const formData = new FormData()
-            formData.append('img', file)
-            const res = await uploadUserAvatar({id, img: file})
-            if (!!res) {
-                await checkAuthApi()
-                // @ts-ignore
-                dispatch(setUser(res.data ? res.data as IUser : null))
-                setChangePhoto(false)
-            }
-        }
+  const uploadUserAvatarHandler = async () => {
+    if (!!file && !!id) {
+      const formData = new FormData();
+      formData.append('img', file);
+      const res = await uploadUserAvatar({ id, img: file });
+      if (!!res) {
+        await checkAuthApi();
+        // @ts-ignore
+        dispatch(setUser(res.data ? (res.data as IUser) : null));
+        setChangePhoto(false);
+      }
     }
-    if (!user) {
-        return null
-    }
+  };
+  if (!user) {
+    return null;
+  }
 
-    return (
-        <div className={styles.PersonalAccountWrap}>
-            <div className={styles.PersonalAccountMainBlock}>
-                <div className={styles.PersonalAccountPhotoBlock}>
-                    <img
-                        className={styles.PersonalAccountPhoto}
-                        src={user && user.avatar ? `http://localhost:5555/` + user!.avatar : fakeAvatar} alt={'avatar'}/>
-                    <div className={styles.ChangePhotoBtn}>
-                        <AppButton
-                            onClick={() => setChangePhoto(true)} text={'Сменить фото'}/>
-                        {changePhoto && (
-                            <ChangeAvatarContainer file={file} setFile={setFile} inputFile={inputFile}
-                                                   setInputFile={setInputFile} changePhoto={changePhoto}
-                                                   setChangePhoto={setChangePhoto} handleChangeFile={handleChangeFile}
-                                                   uploadUserAvatarHandler={uploadUserAvatarHandler}/>
-                        )}
-                    </div>
-                </div>
-                <ProfileInfoContainer
-                    user={user}
-                />
-            </div>
+  return (
+    <div className={styles.PersonalAccountWrap}>
+      <div className={styles.PersonalAccountMainBlock}>
+        <div className={styles.PersonalAccountPhotoBlock}>
+          <img
+            className={styles.PersonalAccountPhoto}
+            src={user && user.avatar ? `http://localhost:5555/` + user!.avatar : fakeAvatar}
+            alt={'avatar'}
+          />
+          <div className={styles.ChangePhotoBtn}>
+            <AppButton onClick={() => setChangePhoto(true)} text={'Сменить фото'} />
+            {changePhoto && (
+              <ChangeAvatarContainer
+                file={file}
+                setFile={setFile}
+                inputFile={inputFile}
+                setInputFile={setInputFile}
+                changePhoto={changePhoto}
+                setChangePhoto={setChangePhoto}
+                handleChangeFile={handleChangeFile}
+                uploadUserAvatarHandler={uploadUserAvatarHandler}
+              />
+            )}
+          </div>
         </div>
-    );
+        <ProfileInfoContainer user={user} />
+      </div>
+    </div>
+  );
 };
