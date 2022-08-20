c0423d4d0d749f75f9480177d39263c2a8ebe4ba
Aloxan12 <kvakvaaak@mail.ru>
2022-08-20 15:34:31 +0300

    eslint

--- client/src/utils/helpers.tsx
+++ client/src/utils/helpers.tsx
@@ -1,37 +1,32 @@
-import {RoleTypes} from "../router/AppRoute";
+import { RoleTypes } from '../router/AppRoute';
 
-export const contentToHtml = (text: string) => text
-    .split(/\n(?!\n)/).filter(item => item !== "")
-    .map((paragraph: string, index:number) => <p key={`${paragraph}- key ${index}`}>{paragraph}</p>);
+export const contentToHtml = (text: string) =>
+  text
+    .split(/\n(?!\n)/)
+    .filter((item) => item !== '')
+    .map((paragraph: string, index: number) => (
+      <p key={`${paragraph}- key ${index}`}>{paragraph}</p>
+    ));
 
-export const getFileType =(fileName?: string) =>{
-    return `${fileName}`
-        .split('')
-        .reverse()
-        .join('')
-        .split('.')[0]
-        .split('')
-        .reverse()
-        .join('')
-}
-
-
+export const getFileType = (fileName?: string) => {
+  return `${fileName}`.split('').reverse().join('').split('.')[0].split('').reverse().join('');
+};
 
 const memoizedIsAdmin = () => {
-    let cache: { [key in string]: boolean } = {}
-    return (role?: RoleTypes): boolean => {
-        if (!!role) {
-            if (role in cache) {
-                return cache[role]
-            } else {
-                const isAdmin = role === RoleTypes.ADMIN
-                cache[role] = isAdmin
-                return isAdmin
-            }
-        } else {
-            return false
-        }
+  let cache: { [key in string]: boolean } = {};
+  return (role?: RoleTypes): boolean => {
+    if (!!role) {
+      if (role in cache) {
+        return cache[role];
+      } else {
+        const isAdmin = role === RoleTypes.ADMIN;
+        cache[role] = isAdmin;
+        return isAdmin;
+      }
+    } else {
+      return false;
     }
-}
+  };
+};
 
-export const useIsAdmin = memoizedIsAdmin()
+export const useIsAdmin = memoizedIsAdmin();
