1cb8fa8e742153a6fa3aeb8fed81a214aa7f67c9
Aloxan12 <kvakvaaak@mail.ru>
2022-08-27 15:00:17 +0300

    throttle

--- client/src/Common/Components/AppInput/AppInput.tsx
+++ client/src/Common/Components/AppInput/AppInput.tsx
@@ -1,91 +1,115 @@
-import React, {ChangeEvent} from 'react';
-import styles from './AppInput.module.css'
-import icoRightPhoto from '../../../utils/images/arrow-down.png'
+import React, { ChangeEvent, useMemo } from 'react';
+import styles from './AppInput.module.css';
+import icoRightPhoto from '../../../utils/images/arrow-down.png';
 
-export enum IcoType{
-    ico_right = 'ico-right'
+export enum IcoType {
+  ico_right = 'ico-right',
 }
 
 export enum InputMaskType {
-    float,
-    integer
+  float,
+  integer,
 }
 
-interface IAppInputBase{
-    label?: string
-    placeholder?: string
-    type?: string
-    value?: string | null
-    onChange: (value: string) => void
-    error?: string | null
-    inputMask?: InputMaskType
+interface IAppInputBase {
+  label?: string;
+  placeholder?: string;
+  type?: string;
+  value?: string | null;
+  onChange: (value: string) => void;
+  error?: string | null;
+  inputMask?: InputMaskType;
+  keyDirectionFn?: null | ((value: React.KeyboardEvent<HTMLInputElement>) => void); // использует исключительно для определения нажатия вверх или вниз на клавиатуре
 }
 
-interface IAppInputTextarea extends IAppInputBase{
-    icoRight?:never
-    rows: number
-    onClick?: never
-    dropdownInput?: never
-    dropdownActive?: never
+interface IAppInputTextarea extends IAppInputBase {
+  icoRight?: never;
+  rows: number;
+  onClick?: never;
+  dropdownInput?: never;
+  dropdownActive?: never;
 }
 
-interface IAppInputIcoRight extends IAppInputBase{
-    icoRight?:IcoType
-    rows?: never
-    onClick?: never
-    dropdownInput?: never
-    dropdownActive?: never
+interface IAppInputIcoRight extends IAppInputBase {
+  icoRight?: IcoType;
+  rows?: never;
+  onClick?: never;
+  dropdownInput?: never;
+  dropdownActive?: never;
 }
 
-interface IAppInputDropdown extends IAppInputBase{
-    icoRight?:never
-    rows?: never
-    onClick: () => void
-    dropdownInput: boolean
-    dropdownActive: boolean
+interface IAppInputDropdown extends IAppInputBase {
+  icoRight?: never;
+  rows?: never;
+  onClick: () => void;
+  dropdownInput: boolean;
+  dropdownActive: boolean;
 }
 
-type AppInputType = IAppInputTextarea | IAppInputIcoRight | IAppInputDropdown
+type AppInputType = IAppInputTextarea | IAppInputIcoRight | IAppInputDropdown;
 
 export const AppInput = ({
-                             label,
-                             type = 'text',
-                             value,
-                             onChange,
-                             onClick,
-                             dropdownInput,
-                             dropdownActive,
-                             error,
-                             placeholder,
-                             icoRight,
-                             inputMask}: AppInputType) => {
-    const wrapperProps = onClick ? { onClick } : {}
+  label,
+  type = 'text',
+  value,
+  onChange,
+  onClick,
+  dropdownInput,
+  dropdownActive,
+  error,
+  placeholder,
+  icoRight,
+  inputMask,
+  keyDirectionFn = null,
+}: AppInputType) => {
+  const wrapperProps = onClick ? { onClick } : {};
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) =>{
-        let result = ''
-        switch (inputMask){
-            case InputMaskType.integer:
-                const integerVal = e.currentTarget.value
-                    .replace(/\D/g, '')
-                    .replace(/^(0)([0-9])+/g, '$2')
-                result = integerVal
-                break
-            default:
-                result = e.currentTarget.value
-        }
-        return  onChange(result)
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    let result = '';
+    switch (inputMask) {
+      case InputMaskType.integer:
+        const integerVal = e.currentTarget.value.replace(/\D/g, '').replace(/^(0)([0-9])+/g, '$2');
+        result = integerVal;
+        break;
+      default:
+        result = e.currentTarget.value;
     }
+    return onChange(result);
+  };
 
-    return (
-        <div className={styles.appInputWrap} {...wrapperProps}>
-            {label && <label>{label}</label>}
-            <div className={`${styles.appInput} ${error ? styles.borderError : ''} ${dropdownActive ? styles.Active : ''}`}>
-                {!!icoRight && icoRight === IcoType.ico_right || !!dropdownInput &&
-                    <img src={icoRightPhoto} alt="arrow-down"  className={`${styles.IcoRight} ${dropdownActive ? styles.Active : ''}`}/>
-                }
-                <input value={!!value ? value : ''} placeholder={placeholder} type={type} onChange={onChangeHandler}/>
-            </div>
-            {error && <div className={styles.appInputError}>{error}</div>}
-        </div>
-    );
-}
+  const keyDownPropsWrapper = useMemo(() => {
+    return keyDirectionFn
+      ? {
+          onKeyDown: keyDirectionFn,
+        }
+      : {};
+  }, [keyDirectionFn]);
+
+  return (
+    <div className={styles.appInputWrap} {...wrapperProps}>
+      {label && <label>{label}</label>}
+      <div
+        className={`${styles.appInput} ${error ? styles.borderError : ''} ${
+          dropdownActive ? styles.Active : ''
+        }`}
+      >
+        {(!!icoRight && icoRight === IcoType.ico_right) ||
+          (!!dropdownInput && (
+            <img
+              src={icoRightPhoto}
+              alt="arrow-down"
+              className={`${styles.IcoRight} ${dropdownActive ? styles.Active : ''}`}
+            />
+          ))}
+        <input
+          value={!!value ? value : ''}
+          placeholder={placeholder}
+          type={type}
+          onChange={onChangeHandler}
+          {...keyDownPropsWrapper}
+        />
+      </div>
+      {error && <div className={styles.appInputError}>{error}</div>}
+    </div>
+  );
+};
