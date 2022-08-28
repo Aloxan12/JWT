96ed7a4e5f8c381a5f8eec21ef7cf6dd7145c076
Aloxan12 <kvakvaaak@mail.ru>
2022-08-28 17:56:05 +0300

    AppDropdown

--- client/src/Common/Components/Tooltip/Tooltip.tsx
+++ client/src/Common/Components/Tooltip/Tooltip.tsx
@@ -1,45 +1,50 @@
-import React, {useState} from "react";
-import styles from './Tooltip.module.css'
+import React, { useState } from 'react';
+import styles from './Tooltip.module.css';
 
 type TooltipType = {
-    delay?: number
-    direction?: string
-    content?: string
-    children?: React.ReactNode
-    textColor?: 'green'
-}
+  delay?: number;
+  direction?: string;
+  content?: string;
+  children?: React.ReactNode;
+  textColor?: 'green';
+};
 
-export const Tooltip = (props: TooltipType)=>{
-    let timeout: ReturnType<typeof setTimeout>;
-    const [active, setActive] = useState(false);
+export const Tooltip = (props: TooltipType) => {
+  let timeout: ReturnType<typeof setTimeout>;
+  const [active, setActive] = useState(false);
 
-    const showTip = () => {
-        timeout = setTimeout(() => {
-            setActive(true);
-        }, props.delay || 400);
-    };
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.delay || 400);
+  };
 
-    const hideTip = () => {
-        clearInterval(timeout);
-        setActive(false);
-    };
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
 
-
-    return (
+  return (
+    <div
+      className={styles.TooltipWrapper}
+      // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {/* Wrapping */}
+      {props.children}
+      {active && (
         <div
-            className={styles.TooltipWrapper}
-            // When to show the tooltip
-            onMouseEnter={showTip}
-            onMouseLeave={hideTip}
+          className={
+            `${styles.TooltipTip} ` +
+            `${props.direction ? styles[props.direction] : styles['top']} ` +
+            `${props.textColor && styles[props.textColor]}`
+          }
         >
-            {/* Wrapping */}
-            {props.children}
-            {active && (
-                <div className={`${styles.TooltipTip} ` + `${props.direction ? styles[props.direction] : styles["top"]} ` + `${props.textColor && styles[props.textColor]}`}>
-                    {/* Content */}
-                    {props.content}
-                </div>
-            )}
+          {/* Content */}
+          {props.content}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
