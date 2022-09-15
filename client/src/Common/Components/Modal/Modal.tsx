630ac1ec87dc4cdd6d994e92600ffcbaf0f77e9a
Aloxan12 <kvakvaaak@mail.ru>
2022-09-15 23:23:40 +0300

    limit for posts

--- client/src/Common/Components/Modal/Modal.tsx
+++ client/src/Common/Components/Modal/Modal.tsx
@@ -1,36 +1,42 @@
-import React, {useState} from "react";
-import style from "./Modal.module.css";
+import React, { useState } from 'react';
+import style from './Modal.module.css';
 
 type ModalType = {
-    active: boolean
-    setActive: (value: boolean) => void
-    title?: string
-}
+  active: boolean;
+  setActive: (value: boolean) => void;
+  title?: string;
+};
 
-export const Modal: React.FC<ModalType> = ({active, setActive, children, title}) => {
-    return (
-        <div className={active ? `${style.modal} + '' + ${style.active}` : style.modal}
-             onClick={() => setActive(false)}>
-            <div className={active ? `${style.modalContent} + '' + ${style.active}` : style.modalContent}
-                 onClick={(e)=> e.stopPropagation()}
-            >
-                {title ?
-                    <React.Fragment>
-                        <div className={style.headerBlock}>{title}</div>
-                        <div className={style.contentBlock}>{children}</div>
-                    </React.Fragment>
-                    :
-                    <div>{children}</div>}
-            </div>
-        </div>
-    )
-}
+export const Modal: React.FC<ModalType> = ({ active, setActive, children, title }) => {
+  return (
+    <div
+      className={active ? `${style.modal} + '' + ${style.active}` : style.modal}
+      onClick={() => setActive(false)}
+    >
+      <div
+        className={active ? `${style.modalContent} + '' + ${style.active}` : style.modalContent}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {title ? (
+          <React.Fragment>
+            <div className={style.headerBlock}>{title}</div>
+            <div className={style.contentBlock}>{children}</div>
+          </React.Fragment>
+        ) : (
+          <div>{children}</div>
+        )}
+      </div>
+    </div>
+  );
+};
 export const ModalView: React.FC = () => {
-    const [active, setActive] = useState<boolean>(false)
-    return (
-        <div>
-            <button onClick={() => setActive(true)}>+</button>
-            <Modal active={active} setActive={setActive}>Модалка</Modal>
-        </div>
-    )
-}
+  const [active, setActive] = useState<boolean>(false);
+  return (
+    <div>
+      <button onClick={() => setActive(true)}>+</button>
+      <Modal active={active} setActive={setActive}>
+        Модалка
+      </Modal>
+    </div>
+  );
+};
