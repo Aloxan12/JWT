f1b0e42bb79d8ba79e9b560fc5085bbbbcfab174
Aloxan12 <kvakvaaak@mail.ru>
2022-09-18 01:09:08 +0300

    Projects page

--- client/src/Common/Components/AppTrash/AppTrash.tsx
+++ client/src/Common/Components/AppTrash/AppTrash.tsx
@@ -1,39 +1,42 @@
-import React, {useState} from 'react';
-import trashIco from '../../../utils/images/trash.png'
-import styles from './AppTrash.module.css'
-import {Modal} from "../Modal/Modal";
-import {AppButton} from "../AppButton/AppButton";
+import React, { useState } from 'react';
+import trashIco from '../../../utils/images/trash.png';
+import styles from './AppTrash.module.css';
+import { Modal } from '../Modal/Modal';
+import { AppButton } from '../AppButton/AppButton';
 
 interface IAppTrashProps {
-    size?: 'big' | 'medium' | 'small'
-    deleteHandler: ()=> void
-    text?: string
+  size?: 'big' | 'medium' | 'small';
+  deleteHandler: () => void;
+  text?: string;
 }
 
-export const AppTrash = ({size, deleteHandler, text}: IAppTrashProps) => {
-    const [active, setActive] = useState<boolean>(false)
+export const AppTrash = ({ size, deleteHandler, text }: IAppTrashProps) => {
+  const [active, setActive] = useState<boolean>(false);
 
-    return (
-        <React.Fragment>
-            <img
-                src={trashIco}
-                alt="корзина"
-                className={styles.trash + " " + `${size ? styles[size] : styles.big}`}
-                onClick={() => setActive(true)}
+  return (
+    <React.Fragment>
+      <img
+        src={trashIco}
+        alt="корзина"
+        className={styles.trash + ' ' + `${size ? styles[size] : styles.big}`}
+        onClick={() => setActive(true)}
+      />
+
+      <Modal active={active} setActive={setActive} title="Предупреждение">
+        <div className={styles.trashModelContent}>
+          <div>{text || 'Удалить?'}</div>
+          <div>
+            <AppButton onClick={() => setActive(false)} text={'нет'} />
+            <AppButton
+              onClick={() => {
+                setActive(false);
+                deleteHandler();
+              }}
+              text={'да'}
             />
-
-            <Modal active={active} setActive={setActive} title="Предупреждение">
-                <div className={styles.trashModelContent}>
-                    <div>{text || 'Удалить?'}</div>
-                    <div>
-                        <AppButton onClick={()=>setActive(false)} text={'нет'}/>
-                        <AppButton onClick={()=> {
-                            setActive(false)
-                            deleteHandler()
-                        }} text={'да'}/>
-                    </div>
-                </div>
-            </Modal>
-        </React.Fragment>
-    );
+          </div>
+        </div>
+      </Modal>
+    </React.Fragment>
+  );
 };
