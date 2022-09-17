5ad532ef34fae7021b4e1e12b9aa84f0217cb94a
Aloxan12 <kvakvaaak@mail.ru>
2022-09-17 12:05:42 +0300

    Projects page

--- client/src/Pages/Users/User.tsx
+++ client/src/Pages/Users/User.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import styles from './Users.module.css'
-import fakeAvatar from '../../utils/images/fake_avatar.png'
+import styles from './Users.module.css';
+import fakeAvatar from '../../utils/images/fake_avatar.png';
 import { IUser } from '../../redux/api/dto/UserDto';
 
 interface IUserProps {
-    user: IUser
+  user: IUser;
 }
 
-export const User = ({user}: IUserProps) => {
-    return (
-        <div key={user.id} className={styles.UserItem}>
-            <div className={styles.UserPhotoBlock}>
-                <img src={!!user.avatar ? `http://localhost:5555/` + user.avatar : fakeAvatar} alt={'avatar'}/>
-            </div>
-            <div>{user.email}</div>
-            <div>Роль: {!!user.role ? user.role : 'Без роли'}</div>
-        </div>
-    )
+export const User = ({ user }: IUserProps) => {
+  return (
+    <div key={user.id} className={styles.UserItem}>
+      <div className={styles.UserPhotoBlock}>
+        <img
+          src={!!user.avatar ? `http://localhost:5555/` + user.avatar : fakeAvatar}
+          alt={'avatar'}
+          loading="eager"
+        />
+      </div>
+      <div>{user.email}</div>
+      <div>Роль: {!!user.role ? user.role : 'Без роли'}</div>
+    </div>
+  );
 };
