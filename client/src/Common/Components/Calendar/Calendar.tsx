92a3bd1355921f9f93a8ac752671fb1598ba1129
Aloxan12 <kvakvaaak@mail.ru>
2022-09-22 15:25:59 +0300

    Projects page

--- client/src/Common/Components/Calendar/Calendar.tsx
+++ client/src/Common/Components/Calendar/Calendar.tsx
@@ -1,6 +1,19 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import styles from './Calendar.module.css';
 import moment from 'moment';
+
+export enum FormatDate {
+  BASE_DATE = 'Y-MM-DD',
+  FULL_DATE = 'Y-MMMM-D',
+  FULL_MONTH = 'MMMM',
+  YEAR = 'Y',
+  DAY = 'D',
+  SHORT_DAY_WEEK = 'dd',
+}
+
+const getAllDays = (count: number) => Array.from({ length: count }, (_, i) => String(++i));
+const getFirstDayMonth = (month: string, year: string) =>
+  moment(`${year}-${month}-1`, FormatDate.FULL_DATE).format(FormatDate.SHORT_DAY_WEEK);
 
 const getStateDate = (val: string) => {
   const date = moment(val);
@@ -13,9 +26,20 @@ const getStateDate = (val: string) => {
 
 const MONTHS = moment.months();
 const NUMBER_MONTHS = 12;
+const DAYS_WEEK = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
 
 export const Calendar = () => {
   const [date, setDate] = useState(getStateDate('2022-09-21T09:12:54+03:00'));
+  const [days, setDays] = useState(getAllDays(moment().daysInMonth()));
+
+  const startDayMonth = useMemo(
+    () => getFirstDayMonth(date.month, date.year),
+    [date.month, date.year]
+  );
+  const daysInMonth = useMemo(
+    () => [...Array.from({ length: DAYS_WEEK.indexOf(startDayMonth) }, (x) => ''), ...days],
+    [startDayMonth, days]
+  );
 
   const handlerChangeMonth = (params: number) => {
     const idx = MONTHS.indexOf(date!.month);
@@ -31,6 +55,25 @@ export const Calendar = () => {
     }
   };
 
+  const renderDays = useCallback(
+    () =>
+      daysInMonth.map((el, idx) => (
+        <div className="item" key={`day-${idx}`}>
+          {el && (
+            <span
+              className={el === date.day ? 'check' : ''}
+              onClick={() => {
+                // handlerChangeDay(el);
+              }}
+            >
+              {el}
+            </span>
+          )}
+        </div>
+      )),
+    [daysInMonth, date.day]
+  );
+
   return (
     <div className={styles.CalendarWrap}>
       <div className="month">
@@ -40,7 +83,16 @@ export const Calendar = () => {
         </span>
         <span onClick={() => handlerChangeMonth(1)}>{'>'}</span>
       </div>
-      <div>lil</div>
+      <div className={styles.WrapperItems}>
+        <div className={styles.DaysWeek}>
+          {DAYS_WEEK.map((name) => (
+            <div className="day" key={name}>
+              {name}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="days">{renderDays()}</div>
     </div>
   );
 };
