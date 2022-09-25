6e78635a9e8e4fb82af66c0c3ba3d5532df4ee6c
Aloxan12 <kvakvaaak@mail.ru>
2022-09-25 13:54:34 +0300

    Calendar

--- client/src/Common/Components/AppRating/AppRating.tsx
+++ client/src/Common/Components/AppRating/AppRating.tsx
@@ -1,42 +1,41 @@
-import React, {useState} from 'react';
-import {Star} from "./Star";
-import styles from './Star.module.css'
+import React, { useState } from 'react';
+import { Star } from './Star';
+import styles from './Star.module.css';
 
 interface IAppRating {
-    value: number
-    onChange: (value: number) => void
+  value: number;
+  onChange: (value: number) => void;
 }
 
 type RatingHelperTextType = {
-    [key in number]: string
-}
+  [key in number]: string;
+};
 
 const ratingHelperText: RatingHelperTextType = {
-    1: 'плохо',
-    2: 'не очень',
-    3: 'норм',
-    4: 'хорошо',
-    5: 'класс',
-}
+  1: 'плохо',
+  2: 'не очень',
+  3: 'норм',
+  4: 'хорошо',
+  5: 'класс',
+};
 
-export const AppRating = ({value, onChange}: IAppRating) => {
-    const [helpText, setHelpText] = useState<number | null>(null)
+export const AppRating = ({ value, onChange }: IAppRating) => {
+  const [helpText, setHelpText] = useState<number | null>(null);
 
-    return (
-        <div className={styles.AppRatingWrap}>
-            {[...Array(5)]
-                .map((item, index) =>
-                    <Star
-                        value={index + 1}
-                        active={value > index}
-                        onChange={onChange}
-                        key={`AppRating ${item} ${index}`}
-                        setHelpText={setHelpText}
-                    />
-                )}
-             <p className={styles.helperText} style={{fontSize: '12px', textAlign: 'center'}}
-            >{!!helpText && ratingHelperText[helpText]}
-            </p>
-        </div>
-    );
+  return (
+    <div className={styles.AppRatingWrap}>
+      {[...Array(5)].map((item, index) => (
+        <Star
+          value={index + 1}
+          active={value > index}
+          onChange={onChange}
+          key={`AppRating ${item} ${index}`}
+          setHelpText={setHelpText}
+        />
+      ))}
+      <p className={styles.helperText} style={{ fontSize: '12px', textAlign: 'center' }}>
+        {!!helpText && ratingHelperText[helpText]}
+      </p>
+    </div>
+  );
 };
