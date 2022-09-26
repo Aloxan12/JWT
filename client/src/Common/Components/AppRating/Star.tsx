eb7bbe45e4f2ff2360f2c795b78930c534f9453b
Aloxan12 <kvakvaaak@mail.ru>
2022-09-26 08:11:38 +0300

    Calendar

--- client/src/Common/Components/AppRating/Star.tsx
+++ client/src/Common/Components/AppRating/Star.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import styles from './Star.module.css'
+import styles from './Star.module.css';
 
 interface IStar {
-    active: boolean
-    value: number
-    onChange: (value: number) => void
-    setHelpText: (value: number | null) => void
+  active: boolean;
+  value: number;
+  onChange: (value: number) => void;
+  setHelpText: (value: number | null) => void;
 }
 
-export const Star = ({active, onChange,setHelpText, value}: IStar) => {
-    return (
-        <div className={styles.wrap}>
-            <div className={styles.wrapStar}>
-                <span
-                    className={`${styles.star} ${active ? styles.active : ''}`}
-                    onClick={() => onChange(value)}
-                    onMouseEnter={()=> setHelpText(value)}
-                    onMouseLeave={()=> setHelpText(null)}
-                >
-                </span>
-            </div>
-        </div>
-    );
+export const Star = ({ active, onChange, setHelpText, value }: IStar) => {
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.wrapStar}>
+        <span
+          className={`${styles.star} ${active ? styles.active : ''}`}
+          onClick={() => onChange(value)}
+          onMouseEnter={() => setHelpText(value)}
+          onMouseLeave={() => setHelpText(null)}
+        ></span>
+      </div>
+    </div>
+  );
 };
