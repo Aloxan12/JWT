b04d8c6550c651ab7f602216fe5fddb599f2b2fd
Aloxan12 <kvakvaaak@mail.ru>
2022-10-01 11:54:12 +0300

    Calendar

--- client/src/redux/api/authApi.ts
+++ client/src/redux/api/authApi.ts
@@ -1,66 +1,66 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
-import {RoleTypes} from "../../router/AppRoute";
-import {IUser} from "./dto/UserDto";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { RoleTypes } from '../../router/AppRoute';
+import { IUser } from './dto/UserDto';
 
 export interface AuthState {
-    accessToken: string | null
-    refreshToken: string | null
-    user: IUser | null
+  accessToken: string | null;
+  refreshToken: string | null;
+  user: IUser | null;
 }
 
 interface ISendRegistration {
-    email: string
-    password: string
-    role: RoleTypes
+  email: string;
+  password: string;
+  role: RoleTypes;
 }
 interface ISendLogin {
-    email: string
-    password: string
+  email: string;
+  password: string;
 }
 
 export const authApi = createApi({
-    reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:5555/api',
-        prepareHeaders: (headers, {getState}) => {
-            // const accessToken = (getState() as RootState).auth.authData.accessToken
-            // const refreshToken = (getState() as RootState).auth.authData.refreshToken
-            const accessToken = localStorage.getItem('token')
-            const refreshToken = localStorage.getItem('refreshToken')
-            if (accessToken || refreshToken) {
-                headers.set('authorization', `Bearer ${accessToken}`)
-                headers.set('refreshToken', `${refreshToken}`)
-            }
-            return headers
-        },
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:5555/api',
+    prepareHeaders: (headers, { getState }) => {
+      // const accessToken = (getState() as RootState).auth.authData.accessToken
+      // const refreshToken = (getState() as RootState).auth.authData.refreshToken
+      const accessToken = localStorage.getItem('token');
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (accessToken || refreshToken) {
+        headers.set('authorization', `Bearer ${accessToken}`);
+        headers.set('refreshToken', `${refreshToken}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ['Users', 'Posts'],
+  endpoints: (build) => ({
+    login: build.mutation<AuthState, ISendLogin>({
+      query: (params) => ({
+        url: '/login',
+        method: 'POST',
+        body: params,
+      }),
+      invalidatesTags: ['Users'],
     }),
-    tagTypes: ['Users', 'Posts'],
-    endpoints: (build) => ({
-        login: build.mutation<AuthState, ISendLogin>({
-            query: (params) => ({
-                url: '/login',
-                method: 'POST',
-                body: params,
-            }),
-            invalidatesTags: ['Users']
-        }),
-        logout: build.mutation<void, void>({
-            query: (params) => ({
-                url: '/logout',
-                method: 'POST',
-                body: params,
-            }),
-            invalidatesTags: ['Users']
-        }),
-        registration: build.mutation<AuthState, ISendRegistration>({
-            query: (params) => ({
-                url: '/registration',
-                method: 'POST',
-                body: params,
-            }),
-            invalidatesTags: ['Users']
-        })
-    })
-})
+    logout: build.mutation<void, void>({
+      query: (params) => ({
+        url: '/logout',
+        method: 'POST',
+        body: params,
+      }),
+      invalidatesTags: ['Users'],
+    }),
+    registration: build.mutation<AuthState, ISendRegistration>({
+      query: (params) => ({
+        url: '/registration',
+        method: 'POST',
+        body: params,
+      }),
+      invalidatesTags: ['Users'],
+    }),
+  }),
+});
 
-export const {useRegistrationMutation, useLoginMutation, useLogoutMutation} = authApi
+export const { useRegistrationMutation, useLoginMutation, useLogoutMutation } = authApi;
