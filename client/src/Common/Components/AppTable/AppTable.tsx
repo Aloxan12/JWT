be1b94d6ca0446f4789d39935960e83596875e48
Aloxan12 <kvakvaaak@mail.ru>
2022-10-06 16:31:31 +0300

    Table

--- client/src/Common/Components/AppTable/AppTable.tsx
+++ client/src/Common/Components/AppTable/AppTable.tsx
@@ -69,7 +69,7 @@ export const AppTable = <T, TKey extends keyof T>({
               return (
                 <th key={`th-table-${index}`}>
                   {th.title}
-                  {!!th.sort && <span>сортировать</span>}
+                  {!!th.sort && <span>сортировать {th.sort.active && th.sort.input}</span>}
                 </th>
               );
             })}
