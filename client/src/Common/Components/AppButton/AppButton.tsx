fe96071cea8851fed0126410393b6bf217535b12
Aloxan12 <kvakvaaak@mail.ru>
2022-11-08 10:09:49 +0300

    ProjectsPage

--- client/src/Common/Components/AppButton/AppButton.tsx
+++ client/src/Common/Components/AppButton/AppButton.tsx
@@ -4,11 +4,12 @@ import styles from './AppButton.module.css';
 interface IAppButtonProps {
   text?: string;
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
 }
 
-export const AppButton = ({ text, onClick }: IAppButtonProps) => {
+export const AppButton = ({ text, onClick, disabled }: IAppButtonProps) => {
   return (
-    <button onClick={onClick} className={styles.AppButton}>
+    <button onClick={onClick} className={styles.AppButton} disabled={disabled}>
       {text || 'Отправить'}
     </button>
   );
