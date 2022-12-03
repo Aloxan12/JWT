2c91f1324b9d1efc834a2c8b72690e8bdc76275c
Aloxan12 <kvakvaaak@mail.ru>
2022-12-03 11:21:26 +0300

    ProjectsPage

--- client/src/Pages/BlankSheet/BlankSheet.tsx
+++ client/src/Pages/BlankSheet/BlankSheet.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './BlankSheet.module.css';
+import { AppInput } from '../../Common/Components/AppInput/AppInput';
 
 export const BlankSheet = () => {
-  return <div></div>;
+  const [input, setInput] = useState('');
+
+  const changeHandler = (value: string) => {
+    setInput(value);
+  };
+
+  return (
+    <div className={styles.BlankSheetWrap}>
+      <AppInput value={input} onChange={changeHandler} type={'textarea'} />
+    </div>
+  );
 };
