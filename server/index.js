222be38e79f3cfcec115977d6ec8c4c0ee1e666c
Aloxan12 <kvakvaaak@mail.ru>
2022-12-06 15:36:58 +0300

    ProjectsPage

--- server/index.js
+++ server/index.js
@@ -1,34 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const fileUpload = require('express-fileupload')
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const mongoose = require('mongoose')
+require("dotenv").config();
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
 
-const router = require('./router/index')
-const errorMiddleware = require('./middlewares/error-middleware')
+const router = require("./router/index");
+const errorMiddleware = require("./middlewares/error-middleware");
 
-path = require('path')
+path = require("path");
 
-const PORT = process.env.PORT || 5000
-const app = express()
+const PORT = process.env.PORT || 5000;
+const app = express();
 
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'uploads')))
-app.use(fileUpload({}))
-app.use(cookieParser())
-app.use(cors({credential: true, origin: process.env.CLIENT_URL}));
-app.use('/api', router)
-app.use(errorMiddleware)
-
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, "uploads")));
+app.use(fileUpload({}));
+app.use(cookieParser());
+app.use(cors({ credential: true, origin: process.env.CLIENT_URL }));
+app.use("/api", router);
+app.use(errorMiddleware);
 
 const startApp = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
-        app.listen(PORT, () => console.log(`connected port - ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
-}
+  try {
+    // await mongoose.connect(process.env.DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+    app.listen(PORT, () => console.log(`connected port - ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
 
-startApp()
+startApp();
