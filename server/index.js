0d57d0a8cb4db0b48c75f312bc043ab3b7e20997
Aloxan12 <kvakvaaak@mail.ru>
2022-12-07 17:50:26 +0300

    ProjectsPage

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const { mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 const router = require("./router/index");
 const errorMiddleware = require("./middlewares/error-middleware");
