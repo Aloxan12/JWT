7589f4cd7d0a177632ceef80cee25cf39365dc1c
Aloxan12 <kvakvaaak@mail.ru>
2022-12-09 10:08:01 +0300

    ProjectsPage

--- client/src/utils/helpers.tsx
+++ client/src/utils/helpers.tsx
@@ -30,3 +30,16 @@ const memoizedIsAdmin = () => {
 };
 
 export const useIsAdmin = memoizedIsAdmin();
+
+function formatBytes(bytes: number, decimals = 2) {
+  if (bytes === 0) {
+    return '0';
+  } else {
+    let k = 1024;
+    let dm = decimals < 0 ? 0 : decimals;
+    let sizes = ['байт', 'КБ', 'МБ', 'ГБ', 'ТБ'];
+    let i = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+  }
+}
+console.log(formatBytes(203194818));
