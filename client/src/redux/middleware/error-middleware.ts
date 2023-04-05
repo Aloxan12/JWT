b0abaf1e48d884397390aad3cc15b09a94200e72
Aloxan12 <kvakvaaak@mail.ru>
2023-04-05 12:35:19 +0300

    table scroll

--- client/src/redux/middleware/error-middleware.ts
+++ client/src/redux/middleware/error-middleware.ts
@@ -1,21 +1,21 @@
-import {isRejectedWithValue, Middleware, MiddlewareAPI } from "@reduxjs/toolkit"
-import {ToastWrapper, ToastWrapperType} from "../../Common/Components/ToastWrapper/ToastWrapper";
-import {logout} from "../Reducers/authReducer/authReducer";
+import { isRejectedWithValue, Middleware, MiddlewareAPI } from '@reduxjs/toolkit';
+import { ToastWrapper, ToastWrapperType } from '../../Common/Components/ToastWrapper/ToastWrapper';
+import { logout } from '../Reducers/authReducer/authReducer';
 
-export const rtkErrorMiddleware: Middleware =
-    (api: MiddlewareAPI) => (next) => (action) => {
-        if (isRejectedWithValue(action)) {
-            console.log('action.payload', action.payload)
-            if (action.payload.status === 401) {
-                api.dispatch(logout())
-            }
-            ToastWrapper({
-                msg: JSON.stringify(
-                    !!action.payload.data ? action.payload.data.message : action.payload,
-                ).replace(/"/g, ''),
-                type: ToastWrapperType.error,
-            })
-        }
-
-        return next(action)
+export const rtkErrorMiddleware: Middleware = (api: MiddlewareAPI) => (next) => (action) => {
+  if (isRejectedWithValue(action)) {
+    console.log('action.payload', action.payload);
+    if (action.payload.status === 401) {
+      history.pushState('', '', 'login');
+      api.dispatch(logout());
     }
+    ToastWrapper({
+      msg: JSON.stringify(
+        !!action.payload.data ? action.payload.data.message : action.payload
+      ).replace(/"/g, ''),
+      type: ToastWrapperType.error,
+    });
+  }
+
+  return next(action);
+};
