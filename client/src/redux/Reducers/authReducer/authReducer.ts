adf7a095104177d8518ab31ddae8b0c9e3e96e19
Aloxan12 <kvakvaaak@mail.ru>
2023-04-23 12:03:51 +0300

    table scroll

--- client/src/redux/Reducers/authReducer/authReducer.ts
+++ client/src/redux/Reducers/authReducer/authReducer.ts
@@ -1,65 +1,59 @@
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
-import {AuthState} from "../../api/authApi";
-import {RoleTypes} from "../../../router/AppRoute";
-import {IUser} from "../../api/dto/UserDto";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AuthState } from '../../api/authApi';
+import { RoleTypes } from '../../../router/AppRoute';
+import { IUser } from '../../api/dto/UserDto';
 
 type AuthInitialStateType = {
-    user: IUser | null,
-    authData: AuthState
-    isAuth: boolean
-}
+  user: IUser | null;
+  authData: AuthState;
+  isAuth: boolean;
+};
 
 const authInitialState: AuthInitialStateType = {
-    user: {
-        email: '',
-        id: '',
-        isActivated: false,
-        role: RoleTypes.USER,
-        avatar: '',
-        status: ''
-    },
-    authData: {
-        accessToken: null,
-        refreshToken: null,
-        user: null
-    },
-    isAuth: false,
-}
+  user: {
+    email: '',
+    id: '',
+    isActivated: false,
+    role: RoleTypes.USER,
+    avatar: '',
+    status: '',
+  },
+  authData: {
+    accessToken: null,
+    refreshToken: null,
+    user: null,
+  },
+  isAuth: false,
+};
 
 const authReducer = createSlice({
-    name: 'auth',
-    initialState: authInitialState as AuthInitialStateType,
-    reducers: {
-        setAuthData: (
-            state,
-            {payload: AuthState}: PayloadAction<AuthState>,
-        ) => {
-            state.authData = AuthState
-            if (AuthState.accessToken !== null && AuthState.refreshToken !== null && AuthState.user) {
-                localStorage.setItem('token', AuthState.accessToken)
-                localStorage.setItem('refreshToken', AuthState.refreshToken)
-                state.isAuth = true
-                state.user = AuthState.user
-            }
-        },
-        setIsAuth: (state,
-                   {payload: isAuth}: PayloadAction<boolean>,) => {
-          state.isAuth= isAuth
-        },
-        setUser: (state,
-                    {payload: user}: PayloadAction<IUser | null>,) => {
-            state.user= user
-        },
-        logout: (state) => {
-            state.isAuth = false
-            state.user = null
-            localStorage.removeItem('token')
-            localStorage.removeItem('refreshToken')
-        },
+  name: 'auth',
+  initialState: authInitialState as AuthInitialStateType,
+  reducers: {
+    setAuthData: (state, { payload: AuthState }: PayloadAction<AuthState>) => {
+      state.authData = AuthState;
+      if (AuthState.accessToken !== null && AuthState.refreshToken !== null && AuthState.user) {
+        localStorage.setItem('token', AuthState.accessToken);
+        localStorage.setItem('refreshToken', AuthState.refreshToken);
+        state.isAuth = true;
+        state.user = AuthState.user;
+      }
     },
-})
+    setIsAuth: (state, { payload: isAuth }: PayloadAction<boolean>) => {
+      state.isAuth = isAuth;
+    },
+    setUser: (state, { payload: user }: PayloadAction<IUser | null>) => {
+      state.user = user;
+    },
+    logout: (state) => {
+      state.isAuth = false;
+      state.user = null;
+      localStorage.removeItem('token');
+      localStorage.removeItem('refreshToken');
+    },
+  },
+});
 
-export const {setAuthData, logout, setIsAuth, setUser} =
-    authReducer.actions
+export const { setAuthData, logout, setIsAuth, setUser } = authReducer.actions;
 
-export default authReducer.reducer
+export default authReducer.reducer;
