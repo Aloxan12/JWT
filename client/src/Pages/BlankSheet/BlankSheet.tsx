3ccd90b6085ae32d5e9600d71fbc7aca1c73fc1a
Aloxan12 <kvakvaaak@mail.ru>
2023-05-17 13:18:26 +0300

    table scroll

--- client/src/Pages/BlankSheet/BlankSheet.tsx
+++ client/src/Pages/BlankSheet/BlankSheet.tsx
@@ -1,6 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ReactDOMServer from 'react-dom/server';
 import './BlankSheet.scss';
 
+const mock = `<h1>какой-то текст</h1>
+        <h2>какой-то текст 2</h2>
+        <br />
+        компонент
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, voluptates?</p>`;
+
 export const BlankSheet = () => {
-  return <div className={'blank-sheet-wrap'}></div>;
+  const [value, setValue] = useState(5);
+  return (
+    <div className={'blank-sheet-wrap'}>
+      <button onClick={() => setValue((prevState) => prevState + 5)}>+5</button>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: mock.replace(
+            'компонент',
+            ReactDOMServer.renderToString(<Counter value={value} />)
+          ),
+        }}
+      />
+    </div>
+  );
+};
+
+interface CounterProps {
+  value: number;
+}
+
+const Counter = ({ value }: CounterProps) => {
+  const [count, setCount] = useState(value);
+
+  useEffect(() => {
+    setCount(value);
+  }, [value]);
+
+  return (
+    <div>
+      <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
+      <div>{count}</div>
+      <button onClick={() => setCount((prevState) => prevState + 1)}>+</button>
+    </div>
+  );
 };
