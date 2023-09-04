6f765a668cf5c734bcec1f76ad104bad90039df9
Aloxan12 <kvakvaaak@mail.ru>
2023-09-04 11:35:03 +0300

    swiper

--- client/src/Pages/BlankSheet/BlankSheet.tsx
+++ client/src/Pages/BlankSheet/BlankSheet.tsx
@@ -1,38 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import './BlankSheet.scss';
 
-interface SliderProps {
-  slides: JSX.Element[];
-}
-
-const AppSlider: React.FC<SliderProps> = ({ slides }) => {
-  const sliderRef = useRef<HTMLDivElement | null>(null);
-
-  return (
-    <div className="slider-container" ref={sliderRef}>
-      <div>
-        <div>Назад</div>
-        <div>Вперед</div>
-      </div>
-      <div className="slider">
-        {slides.map((item, index) => {
-          return (
-            <div className="item" key={index}>
-              {index}
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
-};
-
-const slides = [<div key={1}>Слайд 1</div>, <div key={2}>Слайд 2</div>, <div key={3}>Слайд 3</div>];
-
 export const BlankSheet = () => {
-  return (
-    <div className="blank-sheet-wrap">
-      <AppSlider slides={slides} />
-    </div>
-  );
+  return <div className="blank-sheet-wrap"></div>;
 };
