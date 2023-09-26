1d01e0e9a29056e245fa31859e882100f305f68f
Aloxan12 <kvakvaaak@mail.ru>
2023-09-26 11:43:11 +0300

    swiper

--- client/src/Pages/BlankSheet/BlankSheet.tsx
+++ client/src/Pages/BlankSheet/BlankSheet.tsx
@@ -2,5 +2,18 @@ import React from 'react';
 import './BlankSheet.scss';
 
 export const BlankSheet = () => {
-  return <div className="blank-sheet-wrap"></div>;
+  return (
+    <div className="blank-sheet-wrap">
+      <div className="sticky-wrapper">
+        <div className="sticky-item">sticky 3</div>
+        <div className="sticky-item">sticky 2</div>
+        <div className="sticky-item">sticky 1</div>
+        <div>2121</div>
+        <div>21</div>
+        <div>21</div>
+        <div>21</div>
+        <div>12</div>
+      </div>
+    </div>
+  );
 };
