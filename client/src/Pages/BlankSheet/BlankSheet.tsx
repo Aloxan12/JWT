4df560a53b3097ccfb9743d34e88355abe1e3ca1
Aloxan12 <kvakvaaak@mail.ru>
2023-10-11 07:45:02 +0300

    swiper

--- client/src/Pages/BlankSheet/BlankSheet.tsx
+++ client/src/Pages/BlankSheet/BlankSheet.tsx
@@ -1,16 +1,42 @@
 import React, { useState } from 'react';
 import './BlankSheet.scss';
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const data = [1, 2, 3, 4, 1, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 
 export const BlankSheet = () => {
-  const [] = useState<[]>([]);
+  const curData = data.map((item) => ({ value: item, active: true }));
+  const [items, setItems] = useState<{ value: number; active: boolean }[]>(curData);
+
+  const handleSort = () => {
+    const sortedItems = [...items].sort();
+    setItems(sortedItems);
+  };
+
+  const onFilter = (even: boolean) => () => {
+    const newArr = items.map((item) =>
+      item.value % 2 === 0 ? { ...item, active: even } : { ...item, active: !even }
+    );
+    setItems(newArr);
+  };
+
   return (
     <div className="blank-sheet-wrap">
-      <div className="list">
-        {data.map((item) => (
-          <div key={item}>{item}</div>
-        ))}
+      <button onClick={handleSort}>Сортировать</button>
+      <button onClick={onFilter(false)}>Только зеденые</button>
+      <button onClick={onFilter(true)}>Только красные</button>
+      <div className="wrap">
+        <div className="list">
+          {items.map((item, index) => (
+            <div
+              className={`item${item.value % 2 === 0 ? ' red' : ''}${
+                item.active ? ' active' : ' inactive'
+              }`}
+              key={`${index}-${item.value}`}
+            >
+              {item.value}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
