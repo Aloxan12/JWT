efd841784b6eca44a9437765b026936625e7e9aa
Aloxan12 <kvakvaaak@mail.ru>
2023-10-24 10:33:10 +0300

    imageHelpers

---
+++ client/src/utils/imageHelpers.ts
@@ -0,0 +1,23 @@
+// Проверить битая ли картинка
+export function checkIfImageExists(url: string) {
+  return new Promise((resolve) => {
+    const img = new Image();
+    img.onload = () => resolve(true);
+    img.onerror = () => resolve(false);
+    img.src = url;
+  });
+}
+
+// Преобразовать ссылку на картинку в File
+export const downloadFile = async (url: string) => {
+  const response = await fetch(url);
+  const blob = await response.blob();
+  return new File([blob], 'file-name.jpg', { type: 'image/jpeg' });
+};
+const downloadAndCreateFiles = async (urlFile: string) => {
+  let file: File | null = null;
+  if (urlFile) {
+    file = await downloadFile(urlFile);
+  }
+  return file;
+};
