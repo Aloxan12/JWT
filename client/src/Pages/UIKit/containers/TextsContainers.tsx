348110d0bb349546fe88b7e51c4950478ad35c6f
Aloxan12 <kvakvaaak@mail.ru>
2023-11-05 13:01:15 +0300

    TextContainers

--- client/src/Pages/UIKit/containers/TextsContainers.tsx
+++ client/src/Pages/UIKit/containers/TextsContainers.tsx
@@ -8,9 +8,14 @@ export const TextsContainers = () => {
     <AppCard max title="Текст" withoutBorder>
       <Flex wrap max gap="16">
         <AppText text="текст" />
+
         <AppText text="текст bold-500" bold="500" />
         <AppText text="текст bold-600" bold="600" />
         <AppText text="текст bold-700" bold="700" />
+
+        <AppText text="текст size-14" size="14" />
+        <AppText text="текст size-16" size="16" />
+        <AppText text="текст size-18" size="18" />
       </Flex>
     </AppCard>
   );
--- client/src/shared/ui/AppText/AppText.tsx
+++ client/src/shared/ui/AppText/AppText.tsx
@@ -3,14 +3,22 @@ import cls from './AppText.module.scss';
 import { classNames, Mods } from '../../lib/classNames/classNames';
 
 type TextBold = '400' | '500' | '600' | '700';
+type TextSize = '14' | '16' | '18';
 
 interface AppTextProps {
   className?: string;
   text: string;
   bold?: TextBold;
+  size?: TextSize;
 }
 
-export const AppText = memo(({ text, className, bold = '400' }: AppTextProps) => {
+export const AppText = memo(({ text, className, size = '16', bold = '400' }: AppTextProps) => {
   const mods: Mods = {};
-  return <p className={classNames(cls.text, mods, [className, cls[`bold-${bold}`]])}>{text}</p>;
+  return (
+    <p
+      className={classNames(cls.text, mods, [className, cls[`bold-${bold}`], cls[`size-${size}`]])}
+    >
+      {text}
+    </p>
+  );
 });
