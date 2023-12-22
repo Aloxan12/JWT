4de77827e8001c068320aa24525dcacf2f60a8c4
Aloxan12 <kvakvaaak@mail.ru>
2023-12-22 17:34:43 +0300

    refactor

--- client/src/widgets/Sidebar/Sidebar.tsx
+++ client/src/widgets/Sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import cls from './Sidebar.module.scss';
 import { classNames, Mods } from '../../shared/lib/classNames/classNames';
+import ReactComponent from '*.svg';
+
+const sidebarItemsList = [{ name: 'name', path: 'path1' }];
 
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,6 +14,11 @@ export const Sidebar = () => {
     [cls.openMenu]: isOpen,
   };
 
+  const itemsList = useMemo(
+    () => sidebarItemsList.map((item) => <SidebarItem item={item} key={item.path} />),
+    [isOpen, sidebarItemsList]
+  );
+
   return (
     <div className={classNames(cls.sidebarWrap, mods, [])}>
       <div className={cls.icoBlock} onClick={onOpenHandler}>
@@ -18,6 +26,16 @@ export const Sidebar = () => {
         <span />
         <span />
       </div>
+
+      <div className={cls.sidebarMenu}></div>
     </div>
   );
+};
+
+interface SidebarItemProps {
+  item: { name: string; path: string; ico?: typeof ReactComponent };
+}
+
+const SidebarItem = ({}: SidebarItemProps) => {
+  return <div></div>;
 };
