1b56ba7067dae102cf6ad49321f01465a05572cb
Aloxan12 <kvakvaaak@mail.ru>
2024-01-11 20:31:32 +0300

    refactor AppDragAndDrop

--- client/src/pages/PersonalAccount/components/ChangeAvatarContainer.tsx
+++ client/src/pages/PersonalAccount/components/ChangeAvatarContainer.tsx
@@ -1,8 +1,10 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent } from 'react';
 import styles from '../PersonalAccount.module.scss';
 import { AppButton } from '../../../Common/Components/AppButton/AppButton';
 import { Modal } from '../../../Common/Components/Modal/Modal';
 import { getFileType } from '../../../utils/helpers';
+import { AppDragAndDropPhoto } from '../../../shared/ui/AppDragAndDrop/AppDragAndDropPhoto';
+import { Flex } from '../../../shared/ui/Flex/Flex';
 
 interface IChangeAvatarProps {
   file: File | null;
@@ -25,71 +27,37 @@ export const ChangeAvatarContainer = ({
   inputFile,
   setInputFile,
 }: IChangeAvatarProps) => {
-  const [drag, setDrag] = useState(false);
-
-  const dragStartHandler = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDrag(true);
-  };
-  const dragLeaveHandler = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    setDrag(false);
-  };
-  const onDropHandler = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const file = e.dataTransfer.files;
-    setFile(file[0]);
-    setDrag(false);
-  };
-
   const fileType = getFileType(file?.name);
 
   return (
     <div>
       <Modal active={changePhoto} setActive={setChangePhoto}>
         <div className={styles.SelectFileBlock}>
-          <div className={styles.DropBlock}>
-            {drag ? (
-              <div
-                className={styles.DropItem + ' ' + styles.DropArea}
-                onDragStart={(e) => dragStartHandler(e)}
-                onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragOver={(e) => dragStartHandler(e)}
-                onDrop={(e) => onDropHandler(e)}
-              >
-                <span>Отпустите файл, чтобы загрузить его</span>
+          <AppDragAndDropPhoto file={file} setFile={setFile} className={styles.dragBlock}>
+            <Flex gap="8" direction="column">
+              <div className={styles.BtnChooseFile}>
+                <input
+                  type="file"
+                  name="file"
+                  id="file"
+                  accept={'image/*'}
+                  value={inputFile}
+                  className={styles.InputFile}
+                  onChange={(e) => {
+                    setInputFile(e.target.value);
+                    handleChangeFile(e);
+                  }}
+                />
+                <label htmlFor="file">Выберите фото</label>
               </div>
-            ) : (
-              <div
-                className={styles.DropItem}
-                onDragStart={(e) => dragStartHandler(e)}
-                onDragLeave={(e) => dragLeaveHandler(e)}
-                onDragOver={(e) => dragStartHandler(e)}
-              >
-                <div className={styles.BtnChooseFile}>
-                  <input
-                    type="file"
-                    name="file"
-                    id="file"
-                    accept={'image/*'}
-                    value={inputFile}
-                    className={styles.InputFile}
-                    onChange={(e) => {
-                      setInputFile(e.target.value);
-                      handleChangeFile(e);
-                    }}
-                  />
-                  <label htmlFor="file">Выберите фото</label>
-                </div>
-                <span>
-                  <p>Или перенесите его сюда.</p>
-                </span>
-                <p className={styles.BottomText}>
-                  Файл должен быть <span className={styles.FileForamt}>JPG, PNG, </span> формата!
-                </p>
-              </div>
-            )}
-          </div>
+              <span>
+                <p>Или перенесите его сюда.</p>
+              </span>
+            </Flex>
+            <p className={styles.BottomText}>
+              Файл должен быть <span className={styles.FileForamt}>JPG, PNG, </span> формата!
+            </p>
+          </AppDragAndDropPhoto>
           {!!file && (
             <React.Fragment>
               {fileType.toLowerCase() === 'jpg' || fileType.toLowerCase() === 'png' ? (
