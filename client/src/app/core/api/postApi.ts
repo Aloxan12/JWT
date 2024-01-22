3c8e34971870cabdf083722fbe0b6e3e7782d20b
Aloxan12 <kvakvaaak@mail.ru>
2024-01-22 07:47:40 +0300

    refactor modal

--- client/src/app/core/api/postApi.ts
+++ client/src/app/core/api/postApi.ts
@@ -8,6 +8,17 @@ export const postApi = authApi.injectEndpoints({
         url: '/posts',
         params: !!params ? params : {},
       }),
+      serializeQueryArgs: ({ endpointName }) => {
+        return endpointName;
+      },
+      merge: (currentCache, newItems) => {
+        console.log('currentCache', currentCache);
+        console.log('newItems', newItems);
+        currentCache.results = newItems.results;
+      },
+      forceRefetch({ currentArg, previousArg }) {
+        return currentArg !== previousArg;
+      },
       providesTags: ['Posts', 'Users'],
     }),
     createPosts: build.mutation<IPost, ICreatePost>({
