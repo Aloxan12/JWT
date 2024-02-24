a7f4db6e3ea8180300649953b3bddfb4e3babaf2
Aloxan12 <kvakvaaak@mail.ru>
2024-02-24 16:16:59 +0300

    refactor

--- client/src/pages/Users/ui/components/User.tsx
+++ client/src/pages/Users/ui/components/User.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { memo } from 'react';
 import cls from '../Users.module.scss';
 import { IUser } from '../../../../app/core/api/dto/UserDto';
 import { Tooltip } from '../../../../shared/ui/Tooltip/Tooltip';
@@ -10,11 +10,17 @@ interface IUserProps {
   user: IUser;
 }
 
-export const User = ({ user }: IUserProps) => {
+export const User = memo(({ user }: IUserProps) => {
   return (
-    <AppCard key={user.id} withoutBorder boxShadow classNameContent={cls.userItem}>
+    <AppCard
+      key={user.id}
+      withoutBorder
+      boxShadow
+      classNameContent={cls.userItem}
+      toContent={`${user.id}`}
+    >
       <div className={cls.userPhotoBlock}>
-        <AppPhoto src={user.avatar || avatarFake} alt={'avatar'} />
+        <AppPhoto src={user.avatar || avatarFake} alt="avatar" />
       </div>
       <Tooltip content={user.email}>
         <div className={cls.UserEmailBlock}>{user.email}</div>
@@ -22,4 +28,4 @@ export const User = ({ user }: IUserProps) => {
       <div>Роль: {!!user.role ? user.role : 'Без роли'}</div>
     </AppCard>
   );
-};
+});
--- client/src/shared/ui/AppCard/AppCard.tsx
+++ client/src/shared/ui/AppCard/AppCard.tsx
@@ -1,10 +1,12 @@
 import React, { ReactNode } from 'react';
 import { classNames, Mods } from '../../lib/classNames/classNames';
 import cls from './AppCard.module.scss';
+import { AppLink } from '../AppLink/AppLink';
 
 interface AppCardProps {
   className?: string;
   classNameContent?: string;
+  toContent?: string;
   title?: string;
   max?: boolean;
   withoutBorder?: boolean;
@@ -20,16 +22,20 @@ export const AppCard = ({
   max,
   withoutBorder,
   boxShadow,
+  toContent,
 }: AppCardProps) => {
   const mods: Mods = {
     [cls.max]: max,
     [cls.boxShadow]: boxShadow,
     [cls.withoutBorder]: withoutBorder,
   };
+
+  const content = <div className={classNames(cls.content, {}, [classNameContent])}>{children}</div>;
+
   return (
     <div className={classNames(cls.card, mods, [className])}>
       {title && <span className={cls.title}>{title}</span>}
-      <div className={classNames(cls.content, {}, [classNameContent])}>{children}</div>
+      {toContent ? <AppLink to={toContent}>{content}</AppLink> : content}
     </div>
   );
 };
