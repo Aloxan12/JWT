3daf15c846da34fd85780765b8aba8dfd63b4eea
Aloxan12 <kvakvaaak@mail.ru>
2024-04-05 20:32:33 +0300

    chat

--- client/src/pages/ChatPage/ui/container/ChatList/ChatList.tsx
+++ client/src/pages/ChatPage/ui/container/ChatList/ChatList.tsx
@@ -4,6 +4,7 @@ import { ChatItem } from './ChatItem';
 import cls from './ChatList.module.scss';
 import { useGetAllChatsQuery } from '../../../../../app/core/api/chatApi';
 import { IUser } from '../../../../../app/core/api/dto/UserDto';
+import { RoleTypes } from '../../../../../app/core/router/AppRouter';
 
 interface ChatListProps {
   currentUser: IUser | null;
@@ -13,6 +14,20 @@ export const ChatList = ({ currentUser }: ChatListProps) => {
   const { data: chatListData } = useGetAllChatsQuery();
   return (
     <Flex direction="column" gap="16" align="start" className={cls.chatList}>
+      <ChatItem
+        users={[
+          {
+            email: 'Общий',
+            id: '1',
+            avatar: '',
+            role: RoleTypes.ADMIN,
+            isActivated: false,
+            status: '',
+          },
+        ]}
+        isActive={false}
+        currentUser={currentUser}
+      />
       {chatListData?.results.map(({ id, users }) => (
         <ChatItem key={id} users={users} isActive={false} currentUser={currentUser} />
       ))}
