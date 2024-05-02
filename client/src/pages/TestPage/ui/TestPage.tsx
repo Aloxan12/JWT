77e3b33192965cbd95a65108b17879bba95a5748
Aloxan12 <kvakvaaak@mail.ru>
2024-05-02 16:41:09 +0300

    testPage

--- client/src/pages/TestPage/ui/TestPage.tsx
+++ client/src/pages/TestPage/ui/TestPage.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import cls from './TestPage.module.scss';
+import { AppButton } from '../../../shared/ui/AppButton/AppButton';
 
 const TestPage = () => {
+  const [active, setActive] = useState(false);
+  const setActiveHandler = () => setActive((prev) => !prev);
+
   return (
     <div className={cls.testPageWrap}>
-      <div className={cls.animationBlock}>
-        <div className={cls.whiteCircle} />
-      </div>
+      <div className={cls.animationBlock}>{active && <div className={cls.whiteCircle} />}</div>
+      <AppButton text={active ? 'Закрыть' : 'Открыть'} onClick={setActiveHandler} />
     </div>
   );
 };
