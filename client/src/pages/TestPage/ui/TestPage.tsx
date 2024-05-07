66bb0c0328c67f57e08a62f6c94e366e7bbaa933
Aloxan12 <kvakvaaak@mail.ru>
2024-05-07 14:27:17 +0300

    testPage

--- client/src/pages/TestPage/ui/TestPage.tsx
+++ client/src/pages/TestPage/ui/TestPage.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import cls from './TestPage.module.scss';
 import { AppButton } from '../../../shared/ui/AppButton/AppButton';
+import { Flex } from '../../../shared/ui/Flex/Flex';
 
 const TestPage = () => {
   const [active, setActive] = useState(false);
+  const [activeTransitionBehavior, setActiveTransitionBehavior] = useState(false);
 
   const setActiveHandler = () => setActive((prev) => !prev);
+  const setActiveTransitionBehaviorHandler = () => setActiveTransitionBehavior((prev) => !prev);
 
   return (
     <div className={cls.testPageWrap}>
@@ -13,8 +16,14 @@ const TestPage = () => {
       <AppButton text={active ? 'Закрыть' : 'Открыть'} onClick={setActiveHandler} />
 
       <div className={cls.scrollWrap}>
+        <div className={cls.progressingBar} />
         <div className={cls.scrollContent}> scrollContent</div>
       </div>
+
+      <Flex direction="column" gap="16" className={cls.transitionBehaviorWrap}>
+        <AppButton title="transition-behavior" onClick={setActiveTransitionBehaviorHandler} />
+        {activeTransitionBehavior && <div className={cls.block}>transition-behavior block</div>}
+      </Flex>
     </div>
   );
 };
