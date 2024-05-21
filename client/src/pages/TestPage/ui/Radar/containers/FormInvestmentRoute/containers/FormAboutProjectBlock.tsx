3b07c3d2176423dcbfb71bd0e9fe16409dff398f
Aloxan12 <kvakvaaak@mail.ru>
2024-05-21 12:18:27 +0300

    Form about project

--- client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormAboutProjectBlock.tsx
+++ client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormAboutProjectBlock.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import cls from '../FormInvestmentRoute.module.scss';
 import { FormInvestmentRouteFooter } from './FormInvestmentRouteFooter';
 import { FormTitleBlock } from './companents/FormTitleBlock';
+import { Flex } from '../../../../../../../shared/ui/Flex/Flex';
+import { AppInput } from '../../../shared/ui/AppInput/AppInput';
+import { AppTextarea } from '../../../shared/ui/AppTextarea/AppTextarea';
+import { AppDropdown } from '../../../shared/ui/AppDropdown/AppDropdown';
 
 interface FormAboutProjectBlockProps {
   prevPage: () => void;
@@ -24,6 +28,47 @@ export const FormAboutProjectBlock = ({ nextPage, prevPage }: FormAboutProjectBl
           title="Общая информация о проекте"
           description="Опишите проект, сроки его реализации, а также задачи, для которых требуется финансовая поддержка"
         />
+        <Flex direction="column" gap="32" className={cls.mt24} max>
+          <AppInput placeholder="Название проекта" showCaption fullWidth />
+          <AppTextarea
+            placeholder="Описание проекта"
+            message="Опишите суть проекта, его цели и планируемый результат"
+            rows="2"
+            fullWidth
+            showCaption
+          />
+          <AppDropdown
+            propName="name"
+            propValue="id"
+            data={[{ id: 1, name: 'name 1' }]}
+            value={null}
+            onChange={() => {}}
+            placeholder="Цель финансирования проекта"
+            fullWidth
+            showCaption
+          />
+          <Flex direction="column" max align="start" gap="16">
+            <FormTitleBlock subtitle="Дата начала проекта" />
+            <Flex gap="32" max>
+              <AppInput placeholder="01.01.2024" fullWidth type="date" />
+              <AppInput
+                placeholder="Длительность реализации проекта, мес"
+                message="Пример: 24"
+                showCaption
+                fullWidth
+              />
+            </Flex>
+          </Flex>
+
+          <Flex direction="column" max gap="16">
+            <FormTitleBlock
+              subtitle="Задачи проекта"
+              description="Опишите задачи, для которых потребуется финансовая поддержка"
+            />
+            <AppTextarea placeholder="Задача №1" rows="2" fullWidth showCaption />
+            <div className={cls.mtbBtn}>btn</div>
+          </Flex>
+        </Flex>
       </div>
       <FormInvestmentRouteFooter
         onPreviousPageHandler={onPrevPageHandler}
--- client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/companents/FormTitleBlock.tsx
+++ client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/companents/FormTitleBlock.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import cls from '../../FormInvestmentRoute.module.scss';
 
 interface FormTitleBlockProps {
-  title: string;
-  description: string;
+  title?: string;
+  subtitle?: string;
+  description?: string;
 }
 
-export const FormTitleBlock = ({ title, description }: FormTitleBlockProps) => {
+export const FormTitleBlock = ({ title, subtitle, description }: FormTitleBlockProps) => {
   return (
     <div className={cls.formTitleBlock}>
-      <div className={cls.title}>{title}</div>
-      <span className={cls.description}>{description}</span>
+      {title && <div className={cls.title}>{title}</div>}
+      {subtitle && <div className={cls.subtitle}>{subtitle}</div>}
+      {description && <span className={cls.description}>{description}</span>}
     </div>
   );
 };
