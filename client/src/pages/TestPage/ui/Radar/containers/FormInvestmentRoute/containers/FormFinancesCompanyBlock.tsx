d8e070ec269026518beb511eebdd61a8b2cec258
Aloxan12 <kvakvaaak@mail.ru>
2024-05-21 11:42:35 +0300

    Form finances company

--- client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormFinancesCompanyBlock.tsx
+++ client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormFinancesCompanyBlock.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import cls from '../FormInvestmentRoute.module.scss';
 import { FormInvestmentRouteFooter } from './FormInvestmentRouteFooter';
 import { FormTitleBlock } from './companents/FormTitleBlock';
+import { Flex } from '../../../../../../../shared/ui/Flex/Flex';
+import { AppInput } from '../../../shared/ui/AppInput/AppInput';
 
 interface FormFinancesCompanyBlockProps {
   prevPage: () => void;
@@ -24,6 +26,30 @@ export const FormFinancesCompanyBlock = ({ prevPage, nextPage }: FormFinancesCom
           title="Финансовые показатели"
           description="Укажите финансовые результаты компании: выручку, прибыль, объем нематериальных активов и основных средств"
         />
+        <Flex direction="column" gap="24" className={cls.mt24}>
+          <Flex gap="32" max>
+            <AppInput placeholder="Выручка за прошлый год, млн руб." showCaption fullWidth />
+            <AppInput placeholder="Прибыль за прошлый год, млн руб." showCaption fullWidth />
+          </Flex>
+          <Flex gap="32" max>
+            <AppInput
+              placeholder="Выручка за все кварталы текущего года, млн руб."
+              showCaption
+              fullWidth
+            />
+            <div className={cls.fullWidth} />
+          </Flex>
+          <AppInput
+            placeholder="Общий объем нематериальных активов на балансе за предыдущий календарный год, млн руб."
+            showCaption
+            fullWidth
+          />
+          <AppInput
+            placeholder="Общий объем основных средств на балансе за предыдущий календарный год, млн руб."
+            showCaption
+            fullWidth
+          />
+        </Flex>
       </div>
       <FormInvestmentRouteFooter
         onPreviousPageHandler={onPrevPageHandler}
