6c523639f34c7112df38cc068c5bb5d2b0733622
Aloxan12 <kvakvaaak@mail.ru>
2024-05-21 11:53:42 +0300

    Form support measures

--- client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormSupportMeasuresBlock.tsx
+++ client/src/pages/TestPage/ui/Radar/containers/FormInvestmentRoute/containers/FormSupportMeasuresBlock.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import cls from '../FormInvestmentRoute.module.scss';
 import { FormInvestmentRouteFooter } from './FormInvestmentRouteFooter';
 import { FormTitleBlock } from './companents/FormTitleBlock';
+import { Flex } from '../../../../../../../shared/ui/Flex/Flex';
+import { AppDropdown } from '../../../shared/ui/AppDropdown/AppDropdown';
+import { AppTextarea } from '../../../shared/ui/AppTextarea/AppTextarea';
 
 interface FormSupportMeasuresBlockProps {
   prevPage: () => void;
@@ -24,6 +27,48 @@ export const FormSupportMeasuresBlock = ({ prevPage, nextPage }: FormSupportMeas
           title="Меры поддержки"
           description="Укажите информацию о наличии у компании особых статусов, получении или участии в различных программах/мерах поддержки"
         />
+        <Flex direction="column" gap="32" className={cls.mt24} max>
+          <AppDropdown
+            label="Особые статусы компании"
+            placeholder="Выберите особые статусы, которые есть у компании"
+            data={[{ id: 1, name: 'name' }]}
+            propName="name"
+            propValue="id"
+            value={null}
+            onChange={() => {}}
+            fullWidth
+          />
+          <AppDropdown
+            label="Программы и меры поддержки, в которых компания участвует сейчас"
+            placeholder="Меры поддержки"
+            data={[{ id: 1, name: 'name' }]}
+            propName="name"
+            propValue="id"
+            value={null}
+            onChange={() => {}}
+            fullWidth
+          />
+          <Flex direction="column" gap="16" max>
+            <AppDropdown
+              label="Укажите информацию о полученных мерах поддержки, которые уже завершены"
+              placeholder="Меры поддержки"
+              data={[{ id: 1, name: 'name' }]}
+              propName="name"
+              propValue="id"
+              value={null}
+              onChange={() => {}}
+              fullWidth
+            />
+            <AppTextarea
+              placeholder="Результат от полученной меры поддержки"
+              message="Укажите название проекта, описание и результаты во время конкретной меры поддержки и результаты от получения конкретной меры поддержки"
+              rows="2"
+              fullWidth
+              showCaption
+            />
+            <div className={cls.mtbBtn}>btn</div>
+          </Flex>
+        </Flex>
       </div>
       <FormInvestmentRouteFooter
         onPreviousPageHandler={onPrevPageHandler}
